253531445b3cea276209e56d1fd1afede70ef9b2
Frank Schmitt <frank@qwhon.de>
2017-12-01 23:19:23 +0100

    2017-01: inverse captcha: first working solution (for toy examples)

--- 2017/01-inverse_captcha/test_inverse_captcha.py
+++ 2017/01-inverse_captcha/test_inverse_captcha.py
@@ -1,8 +1,18 @@
 
 def inverse_captcha(n):
-  return 0
+  #s = str(n) + str(n)[0] # append first digit at end
+  sum = 0
+  s = str(n)
+  for index, elem in enumerate(s):
+    # compare with next elem; at end of string: re-use first element
+    if elem == s[(index + 1) % len(s)]:
+      sum += int(elem)
+  return sum
 
 
 # UNIT TESTS
 def test_inverse_captcha_for_two_different_digits_equals_0():
   assert( inverse_captcha(12) == 0 )
+
+def test_inverse_captcha_for_1122_equals_3():
+  assert( inverse_captcha(1122) == 3 )
