e298f1aef8bc1b070382c011bfc52b25123e29fd
Frank Schmitt <frank@qwhon.de>
2017-12-02 23:49:02 +0100

    2017-02: corruption checksum: first draft of computing row checksum (with pattern matching)

--- 2017/02-corruption_checksum/src/main.rs
+++ 2017/02-corruption_checksum/src/main.rs
@@ -1,10 +1,24 @@
 #[cfg(test)]
+
 mod tests {
+
+  fn corruption_checksum(vals: Vec<i32>) -> i32 {
+    let biggest = vals.iter().max();
+    let smallest = vals.iter().min();
+    match (biggest, smallest) {
+      (Some(a), Some (b)) => a - b,
+      _ => -1
+    } 
+  }
+
+
   #[test]
-  fn it_works() {
-    assert!(1 == 2)
+  fn checksum_should_return_max_minus_min_for_two_elem_matrix() {
+    assert_eq!(corruption_checksum(vec![1,5]), 4);
+    assert_eq!(corruption_checksum(vec![1,4]), 3);
   }
 }
+
 
 fn main() {
     println!("Hello, world!");
