6799ad2e7abe9f80531630e151404a1c88abaaf2
Frank Schmitt <frank@qwhon.de>
2017-12-03 15:26:07 +0100

    2017-02: corruption challenge: part II solved

--- 2017/02-corruption_checksum/src/main.rs
+++ 2017/02-corruption_checksum/src/main.rs
@@ -1,5 +1,8 @@
 #[cfg(test)]
 
+// problem: corruption checksum
+// language: Rust
+
 mod tests {
 
   type Row = Vec<u32>;
@@ -21,6 +24,26 @@ mod tests {
     mat.iter().fold(0u32, |sum, val| sum + row_checksum(val.to_vec()))
   }
 
+  // compute the division result for a single row
+  // returns a/b for the two values in row for which "a is evenly divided by b" holds
+  fn row_division(row: &Row) -> u32 { 
+     // this is terribly inefficient: n**2 where n denotes the number of elements in row
+     for x in row {
+       for y in row {
+         if ((x % y) == 0) && (x != y) {
+            return x / y;
+         } 
+       }
+     } 
+     0 // dummy value - I'm too lazy to correctly handle wrong inputs
+  }
+
+  // compute the division result for a matrix by computing the sum of the row divisions
+  fn matrix_division(mat: &Matrix) -> u32 {
+    mat.iter().fold(0u32, |sum, val| sum + row_division(val))
+  }
+
+  // TESTS
   #[test]
   fn row_checksum_should_return_max_minus_min() {
     assert_eq!(row_checksum(vec![1,5]), 4);
@@ -39,8 +62,24 @@ mod tests {
   }
 
   #[test]
-  fn solve_puzzle() {
-  let input = vec![
+  fn row_division_should_return_division_result() {
+    assert_eq!(row_division(&vec![5,9,2,8]), 4);
+    assert_eq!(row_division(&vec![5,9,4,8]), 2);
+  }
+
+  #[test]
+  fn matrix_division_checksum_should_return_sum_of_division_results_for_example() {
+    let input = vec![
+      vec![5, 9, 2, 8],
+      vec![9, 4, 7, 3],
+      vec![3, 8, 6, 5]
+     ];
+    // (8 / 2) + (9 / 3) + (6 / 3) = 4 + 3 + 2 = 9
+    assert_eq!(matrix_division(&input), 9);
+  }
+
+  fn challenge_input() -> Matrix {
+   vec![
     vec![ 62,1649,1731,76,51,1295,349,719,52,1984,2015,2171,981,1809,181,1715],
     vec![ 161,99,1506,1658,84,78,533,242,1685,86,107,1548,670,960,1641,610],
     vec![ 95,2420,2404,2293,542,2107,2198,121,109,209,2759,1373,1446,905,1837,111],
@@ -57,8 +96,13 @@ mod tests {
     vec![ 271,348,229,278,981,1785,2290,516,473,2037,737,2291,2521,1494,1121,244],
     vec![ 2208,2236,1451,621,1937,1952,865,61,1934,49,1510,50,1767,59,194,1344],
     vec![ 94,2312,2397,333,1192,106,2713,2351,2650,2663,703,157,89,510,1824,125]
-  ];
-  assert_eq!(matrix_checksum(input), 44216);
+  ]
+  }
+
+  #[test]
+  fn solve_puzzle() {
+  assert_eq!(matrix_checksum(challenge_input()), 44216);
+  assert_eq!(matrix_division(&challenge_input()), 320);
  } 
 }
 
