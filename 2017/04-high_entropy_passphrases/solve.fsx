f5cacfd12c3c649f1ec6a3852f1e59df0bc3a2c7
Frank Schmitt <frank@qwhon.de>
2017-12-04 22:34:53 +0100

    2017-04: high entropy passphrases: solved part I

--- 2017/04-high_entropy_passphrases/solve.fsx
+++ 2017/04-high_entropy_passphrases/solve.fsx
@@ -5,7 +5,7 @@ open System
 
 let split separators (x:string) = x.Split(separators)
 
-let checkPassphrase s =
+let isValidPassphrase s =
   //let words = split [|' '|] s
   let duplicates = s |> split [|' '|] 
                      |> Seq.groupBy id
@@ -21,12 +21,20 @@ let assertEquals expected actual msg =
 
 // poor man's test helper
 let testPassphrase input expected = 
-  let check = checkPassphrase(input)
+  let check = isValidPassphrase(input)
   let s = assertEquals expected check input
   printfn "%s" s 
 
 // unit tests
 [ ("abc", true)
 ; ("abc abc", false)
+; ("abc def abc", false)
+; ("aa bb cc dd aaa", true)
 ]
 |> List.iter (fun (input,expected) -> testPassphrase input expected)
+
+// solution for part I: 466
+let input = System.IO.File.ReadLines("input.txt")
+Seq.filter (fun s -> isValidPassphrase s) input
+|> Seq.length
+|> printfn "%d"
