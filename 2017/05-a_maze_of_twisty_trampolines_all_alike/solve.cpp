c5b2ee66de09f1852206e11d89820f554c735507
Frank Schmitt <frank@qwhon.de>
2017-12-06 19:20:27 +0100

    2017-05: maze of twisty trampolines: solution for part II

--- 2017/05-a_maze_of_twisty_trampolines_all_alike/solve.cpp
+++ 2017/05-a_maze_of_twisty_trampolines_all_alike/solve.cpp
@@ -40,6 +40,31 @@ int num_jumps(vector<int>& input) {
   return n;
 }
 
+// perform the next step by computing the next jump target and incrementing the current cell
+void next_step2(int& current, vector<int>& work) {
+  int next = current + work[current];
+  if (work[current] >= 3) {
+    work[current] = work[current] - 1;
+  }
+  else {
+    work[current] = work[current] + 1;
+  }
+  current = next;
+}
+
+// compute the number of jumps needed to escape the maze
+int num_jumps2(vector<int>& input) {
+  int current = 0;
+  int n = 0;
+  int size = input.size();
+  while (current < size) {
+    //std::cerr << "size: " << size << ", n: " << n << ", current: " << current << ", input: " << to_s(input, current) << "\n";
+    ++n;
+    next_step2(current, input);
+  }
+  return n;
+}
+
 void assert_equals(const vector<int>& expected, const vector<int>& actual, const string& msg) {
   if (to_s(expected) == to_s(actual)) {
     std::cout <<  ".";
@@ -94,6 +119,15 @@ void num_jumps_for_example_should_return_5() {
   assert_equals(5, num_jumps(input), "num jumps for example");
 }
 
+void num_jumps2_for_example_should_return_10() {
+  int helper[5] = { 0, 3, 0, 1, -3 };
+  vector<int> input (helper, helper+5);
+  assert_equals(10, num_jumps2(input), "num jumps2 for example");
+  int helper2[5] = { 2, 3, 2, 3, -1 };
+  vector<int> expected (helper2, helper2+5);
+  assert_equals(expected, input, "offsets");
+}
+
 // read the input file into a vector of ints
 std::vector<int> read_input() {
   std::ifstream is("input.txt");
@@ -115,8 +149,12 @@ int main() {
   next_step_for_example_should_correctly_set_offsets_and_current();
   next_step_for_example_step3_should_correctly_set_offsets_and_current();
   num_jumps_for_example_should_return_5();
+  num_jumps2_for_example_should_return_10();
   std::vector<int> input = read_input();
   std::cout << "num jumps for part I: " << num_jumps(input) << "\n";
+  // re-read input, because num_jumps() performs destructive updates on input
+  std::vector<int> input2 = read_input();
+  std::cout << "num jumps for part II: " << num_jumps2(input2) << "\n";
   return 0;
 }
 
