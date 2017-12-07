b937e8cbaf5bbf646174fce83533c4b5c4f0557a
Frank Schmitt <frank@qwhon.de>
2017-12-07 20:59:03 +0100

    2017-06 memory reallocation: solved part I

--- 2017/06-memory_reallocation/memory_reallocation.jl
+++ 2017/06-memory_reallocation/memory_reallocation.jl
@@ -2,7 +2,6 @@ using Base.Test
 
 # choose the next block for redistribution
 # @return the value and index of the block to distribute (1 based)
-#
 function choose_block(x)
   findmax(x)
 end
@@ -22,6 +21,20 @@ function redistribute(x)
   result
 end
 
+# count redistributions until cycle is detected
+# @return the number of redistributions
+function count_until_cycle(x)
+  seen = Dict()
+  n = 0
+  next = x
+  while !haskey(seen, "$next")
+    n += 1
+    seen["$next"] = true
+    next = redistribute(next)
+  end
+  n
+end
+
 # tests for choose_block 
 @test choose_block([0, 2, 7, 0]) == (7,3)
 # choose_block should choose the first one if multiple max-value blocks exist
@@ -30,3 +43,11 @@ end
 # tests for redistribution
 @test redistribute([0, 0, 0, 1]) == [1, 0, 0, 0]
 @test redistribute([0, 2, 7, 0]) == [2, 4, 1, 2]
+
+# tests for detecting cycles
+@test count_until_cycle([0, 2, 7, 0]) == 5
+
+
+# solve it
+input = [11,11,13,7,0,15,5,5,4,4,1,1,7,1,15,11]
+println(count_until_cycle(input))
