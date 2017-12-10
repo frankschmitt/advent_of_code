230b08ba8ed28cb79fdb282a88efa130cb6d993d
Frank Schmitt <frank@qwhon.de>
2017-12-10 14:45:33 +0100

    2017-07 recursive circus: added Tree class for holding the complete tree (start of part II)

--- 2017/07-recursive_circus/lib/recursive_circus.cr
+++ 2017/07-recursive_circus/lib/recursive_circus.cr
@@ -1,12 +1,29 @@
 class Node
-  property name, weight, children
+  property name, weight, child_names
 
-  def initialize(name : String, weight : Int32, children : Array(String))
-    @name, @weight, @children = name, weight, children
+  def initialize(name : String, weight : Int32, child_names : Array(String))
+    @name, @weight, @child_names = name, weight, child_names
   end
 
   def ==(other : Node)
-    @name == other.name && @weight == other.weight && @children == other.children
+    @name == other.name && @weight == other.weight && @child_names == other.child_names
+  end
+end
+
+
+class Tree
+  property root, children
+
+  def initialize(root : Node, children : Array(Tree))
+    @root, @children = root, children
+  end
+
+  def ==(other : Tree)
+    @root == other.root && children == other.children
+  end
+
+  def to_s
+    "#{root.name} (#{root.weight}) -> #{children.map {|c| c.root.name}.join(',') }"
   end
 end
 
@@ -31,7 +48,7 @@ class RecursiveCircus
     Node.new(name, weight, children)
   end
 
-  # parse input, returning the list of Node instances
+  # parse input, returning the list of Node instances (no tree building so far)
   def parse_input(input)
     nodes = [] of Node
     input.each do |line|
@@ -45,8 +62,10 @@ class RecursiveCircus
     # extract list of nodes
     nodes = parse_input(input)
     # build shallow list of children
-    all_children = nodes.map {|n| n.children}.flatten
+    all_children = nodes.map {|n| n.child_names}.flatten
     # find the node that is not a child (= the root node)
     nodes.find { |n| !all_children.includes?(n.name) }
   end
+
+
 end
