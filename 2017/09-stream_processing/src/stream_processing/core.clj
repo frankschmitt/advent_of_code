82ede26e7a21dcf636329cd6a1cb2c1ccbbfc0bc
Frank Schmitt <frank@qwhon.de>
2017-12-14 22:06:27 +0100

    2017-09 stream processing: first function

--- 2017/09-stream_processing/src/stream_processing/core.clj
+++ 2017/09-stream_processing/src/stream_processing/core.clj
@@ -1,6 +1,11 @@
 (ns stream-processing.core
   (:gen-class))
 
+(defn score
+  "Computes and returns the score for an input string"
+  [input]
+  (+ 0 1))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
--- 2017/09-stream_processing/test/stream_processing/core_test.clj
+++ 2017/09-stream_processing/test/stream_processing/core_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [stream-processing.core :refer :all]))
 
-(deftest a-test
+(deftest score-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 1 (score "{}")))))
