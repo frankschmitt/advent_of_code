2cb91cd10f2ed43d9c87b33397d3ba8a336a4440
Frank Schmitt <frank@qwhon.de>
2017-12-15 19:05:05 +0100

    2017-09 stream processing: basic implementation of score, no handling of junk yet

--- 2017/09-stream_processing/StreamProcessing.hs
+++ 2017/09-stream_processing/StreamProcessing.hs
@@ -1,14 +1,15 @@
 module StreamProcessing where
 
--- given the (remaining) input and a partial score so far, compute the overall score
-score' :: [Char] -> Int -> Int
-score' "" n = n
-score' (x:xs) n = 
+-- given the (remaining) input, a nesting depth, and a partial score so far, compute the overall score
+score' :: [Char] -> Int -> Int -> Int
+score' "" _ n = n
+score' (x:xs) depth n = 
   case x of
-    '{' -> score' xs (  n + 1)
-    _   -> score' xs n
+    '{' -> score' xs (depth + 1) n
+    '}' -> score' xs (depth - 1) (n + depth) -- we update the score on closing brackets
+    _   -> score' xs depth n 
  
 -- compute the score for the given input string
 score :: [Char] -> Int
-score input = score' input 0 
+score input = score' input 0 0 
 
--- 2017/09-stream_processing/_StreamProcessing.hs
+++ 2017/09-stream_processing/_StreamProcessing.hs
@@ -5,10 +5,21 @@ import Test.HUnit
 import StreamProcessing
 
 testScoreForSingleBracketPair  = test [ "score for single pair of brackets" ~: 1 ~=? score "{}" ]
+
 testScoreForNestedBrackets  = test [ "score for nested brackets" ~: 6 ~=? score "{{{}}}" ]
+
+testScoreForSequenceOfBrackets  = test [ "score for sequence of brackets" ~: 5 ~=? score "{{},{}}" ]
+
+testScoreWithJunk = test [ "score with junk" ~: 1 ~=? score "{<a>,<a>,<a>,<a>}"]
+
+-- {{<ab>},{<ab>},{<ab>},{<ab>}}, score of 1 + 2 + 2 + 2 + 2 = 9.
+--{{<!!>},{<!!>},{<!!>},{<!!>}}, score of 1 + 2 + 2 + 2 + 2 = 9.
+--{{<a!>},{<a!>},{<a!>},{<ab>}}, score of 1 + 2 = 3.
 
 tests = TestList [ testScoreForSingleBracketPair
                  , testScoreForNestedBrackets 
+                 , testScoreForSequenceOfBrackets
+                 , testScoreWithJunk
                  ] 
 
 main = do runTestTT tests
