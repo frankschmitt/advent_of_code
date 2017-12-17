a5359c1104fec68978b352dc23f1b76197bb1c98
Frank Schmitt <frank@qwhon.de>
2017-12-17 11:08:22 +0100

    2017-10 knot hash: solution for base case (without wrapping around)

--- 2017/10-knot_hash/src/knot_hash/core.clj
+++ 2017/10-knot_hash/src/knot_hash/core.clj
@@ -1,13 +1,34 @@
 (ns knot-hash.core
   (:gen-class))
 
+(defrecord KnotHashState [input, position, skip, lengths])
+
+;;(defrecord Person [fname lname address])
+
+(defn step
+  "Given the current state and the list of input lengths, compute the next step"
+  [old-state]
+  (let [offset (first (:lengths old-state))
+        ;old-input (cycle (:input old-state)) ; use cycle to wrap around
+        old-input (:input old-state) ; use cycle to wrap around
+        old-pos (:position old-state)
+        length (count old-input)
+        ; construct new input
+        ;seg1 (take old-pos old-input) ; first part: unchanged
+        ;seg2 (take offset (drop old-pos old-input)) ; second part: reversed
+        ;seg3 (take length (drop (+ offset old-pos) old-input)) ; third part: unchanged (too long, but we'll fix that in the next step)
+        ;new-input (take length (concat seg1 (reverse seg2) seg3))
+
+        ;new-input (concat (take offset old-input) (reverse (drop offset old-input)))
+        new-input (concat (reverse (take offset old-input)) (drop offset old-input))
+
+        new-pos (mod (+ old-pos offset) length)
+        new-skip (+ 1 (:skip old-state))
+        new-lengths (rest (:lengths old-state))
+        ]
+    (->KnotHashState new-input new-pos new-skip new-lengths)))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
-
-(defn step
-  "Given the current state and the list of input lengths, compute the next step"
-  [state, lengths]
-  (let [offset (first lengths)]
-  (concat (reverse (take offset state)) (drop offset state))))
--- 2017/10-knot_hash/test/knot_hash/core_test.clj
+++ 2017/10-knot_hash/test/knot_hash/core_test.clj
@@ -2,6 +2,30 @@
   (:require [clojure.test :refer :all]
             [knot-hash.core :refer :all]))
 
-(deftest step-should-set-next
+(deftest step-should-work-for-no-wrap
   (testing "first step in example")
-  (is (= '(2 1 0 3 4) (step '(0 1 2 3 4) '(3 4 1 5)))))
+  (let [input '(0 1 2 3 4)
+        lengths '(3 4 1 5)
+        pos 0
+        skip 0
+        start-state (knot-hash.core/->KnotHashState input pos skip lengths)
+        ;; reverse first 3 elements, inc pos by 3, inc skip by 1, pop lengths
+        expected-state (knot-hash.core/->KnotHashState '(2 1 0 3 4) 3 1  '(4 1 5))
+        ]
+    (is (= expected-state (step start-state)))))
+
+; The second length, 4, selects a section which wraps: 2 1) 0 ([3] 4.
+; The sublist 3 4 2 1 is reversed to form 1 2 4 3: 4 3) 0 ([1] 2.
+; The current position moves forward by the length plus the skip size, a total of 5, causing it not to move because it wraps around: 4 3 0 [1] 2
+; The skip size increases to 2.
+(deftest step-should-work-with-wrap
+  (testing "second step in example")
+  (let [input '(2 1 0 3 4)
+        lengths '(4 1 5)
+        pos 3
+        skip 1
+        start-state (knot-hash.core/->KnotHashState input pos skip lengths)
+        ;; reverse 3 4 2 1, inc pos by 3+2, inc skip by 1, pop lengths
+        expected-state (knot-hash.core/->KnotHashState '(4 3 0 1 2) 3 2  '(1 5))
+        ]
+    (is (= expected-state (step start-state)))))
