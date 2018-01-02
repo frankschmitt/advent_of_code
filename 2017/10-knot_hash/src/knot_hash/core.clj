93840428b238882af378aafe499ad653f03baa5f
Frank Schmitt <frank@qwhon.de>
2018-01-02 23:28:58 +0100

    2017-10 knot hash: basic algorithm working (with vectors instead of lists), also fixed stupid bug in unit test

--- 2017/10-knot_hash/src/knot_hash/core.clj
+++ 2017/10-knot_hash/src/knot_hash/core.clj
@@ -3,21 +3,33 @@
 
 (defrecord KnotHashState [input, position, skip, lengths])
 
+;; Example:
+;;(assoc-multiple [1 2 3 4 5 6 7 8] [0 1] [10 20])
+;;   => [10 20 3 4 5 6 7 8]
+(defn assoc-multiple
+  "Replaces multiple values in a vector (given by index and value lists)"
+  [v indices values]
+  (->> (interleave indices values)
+       (apply assoc v)))
+
+
 (defn hash-step
   "Given input, starting pos and length, reverse the substring starting at pos and having the given length (with wrap-around"
   [old-state]
   (let [ old-input (:input old-state)
-         ;wrapped-input (wrap-input old-state)
          wrapped-input (cycle old-input)
          overall_length (count old-input)
          seg1_length (:position old-state)
          seg2_length (first (:lengths old-state))
-         seg3_length (- overall_length seg1_length seg2_length)
-         seg1 (take seg1_length old-input)
+         ;seg3_length (- overall_length seg1_length seg2_length)
+         ;seg1 (take seg1_length old-input)
          seg2 (reverse (take seg2_length (drop seg1_length wrapped-input)))
-         seg3 (take seg3_length (drop (+ seg1_length seg2_length) wrapped-input))
+         ;seg3 (take seg3_length (drop (+ seg1_length seg2_length) wrapped-input))
+         ;; indices from seg1_length+1 .. seg1_length+seg2_length, modulo overall_length (= segment we want to replace)
+         indices-to-switch (map #(mod (+ seg1_length (- %1 1)) overall_length) (range 1 (inc seg2_length)))
         ]
-        (concat seg1 seg2 seg3)))
+        ;(concat seg1 seg2 seg3)))
+        (assoc-multiple old-input indices-to-switch seg2)))
 
 (defn step
   "Given the current state and the list of input lengths, compute the next step"
--- 2017/10-knot_hash/test/knot_hash/core_test.clj
+++ 2017/10-knot_hash/test/knot_hash/core_test.clj
@@ -4,39 +4,40 @@
 
 (deftest hash-step-first-iteration
   (testing "hashing, first step in example")
-  (let [input '(0 1 2 3 4)
+  (let [input [0 1 2 3 4]
         lengths '(3 4 1 5)
         pos 0
         skip 0
         start-state (knot-hash.core/->KnotHashState input pos skip lengths)
         ;; reverse first 3 elements, inc pos by 3, inc skip by 1, pop lengths
-        expected '(2 1 0 3 4)
+        expected [2 1 0 3 4]
         ]
     (is (= expected (hash-step start-state)))))
 
 (deftest hash-step-second-iteration
   (testing "hashing, second step in example"
-    (let [input '(2 1 0 3 4)
+    (let [input [2 1 0 3 4]
           lengths '(4)
           pos 3
           skip 1
           start-state (knot-hash.core/->KnotHashState input pos skip lengths)
-          expected '(3 4 2 1 0)
+          expected [4 3 0 1 2]
           ]
       (is (= expected (hash-step start-state))))))
-      ;(= 1 1 ))))
+      ;(is (= 1 4 )))))
 
 (deftest step-should-work-for-no-wrap
   (testing "first step in example")
-  (let [input '(0 1 2 3 4)
+  (let [input [0 1 2 3 4]
         lengths '(3 4 1 5)
         pos 0
         skip 0
         start-state (knot-hash.core/->KnotHashState input pos skip lengths)
         ;; reverse first 3 elements, inc pos by 3, inc skip by 1, pop lengths
-        expected-state (knot-hash.core/->KnotHashState '(2 1 0 3 4) 3 1  '(4 1 5))
+        expected-state (knot-hash.core/->KnotHashState [2 1 0 3 4] 3 1  '(4 1 5))
         ]
     (is (= expected-state (step start-state)))))
+    ;(is (= 1 3))))
 
 ; The second length, 4, selects a section which wraps: 2 1) 0 ([3] 4.
 ; The sublist 3 4 2 1 is reversed to form 1 2 4 3: 4 3) 0 ([1] 2.
@@ -44,13 +45,13 @@
 ; The skip size increases to 2.
 (deftest step-should-work-with-wrap
   (testing "second step in example")
-  (let [input '(2 1 0 3 4)
+  (let [input [2 1 0 3 4]
         lengths '(4 1 5)
         pos 3
         skip 1
         start-state (knot-hash.core/->KnotHashState input pos skip lengths)
         ;; reverse 3 4 2 1, inc pos by 3+2, inc skip by 1, pop lengths
-        expected-state (knot-hash.core/->KnotHashState '(4 3 0 1 2) 3 2  '(1 5))
+        expected-state (knot-hash.core/->KnotHashState [4 3 0 1 2] 3 2  '(1 5))
         ]
-    ;(is (= expected-state (step start-state)))))
-    (is (= 1 1))))
+    (is (= expected-state (step start-state)))))
+    ;(is (= 1 2))))
