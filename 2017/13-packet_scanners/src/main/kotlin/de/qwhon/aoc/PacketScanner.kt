2b5569b5ab734469851c421a8ed6fe376d21a235
Frank Schmitt <frank@qwhon.de>
2018-01-12 23:12:02 +0100

    2017-13 packet scanners: removed obsolete instance functions + tests

--- 2017/13-packet_scanners/src/main/kotlin/de/qwhon/aoc/PacketScanner.kt
+++ 2017/13-packet_scanners/src/main/kotlin/de/qwhon/aoc/PacketScanner.kt
@@ -2,31 +2,9 @@ package de.qwhon.aoc
 
 import java.io.File
 
-data class ScanningLayer(
-        val range: Int,
-        var scannerDirection: Int = 1, // 1: upward, -1: downward
-        var scannerPosition: Int = 0   // starting position
-) {
-    companion object {
-        val UPWARD = 1
-        val DOWNWARD = -1;
-    }
-
-    fun move() {
-        if (scannerDirection == UPWARD && scannerPosition == (range - 1)) {
-            scannerDirection = DOWNWARD
-        }
-        if (scannerDirection == DOWNWARD && scannerPosition == 0) {
-            scannerDirection = UPWARD
-        }
-        scannerPosition += scannerDirection
-    }
-}
-
 data class ScannerRecord(val depth: Int, val range: Int)
 
-data class PacketScanner(var currentLayerOfPacket: Int, val layers: Map<Int, ScanningLayer>,
-                         var violations: HashMap<Int,Int> = hashMapOf()) {
+class PacketScanner {
 
     companion object {
 
@@ -43,17 +21,6 @@ data class PacketScanner(var currentLayerOfPacket: Int, val layers: Map<Int, Sca
             return result
         }
 
-        /** create a scanner for the given input file.
-         *
-         * @param fileName Name of the input file containing the layer specs (format: <layer>: <depth>, e.g. "0: 4")
-         * @return packet scanner with initial state
-         */
-        fun getScannerForInputFile(fileName: String): PacketScanner {
-            val input = parseInputFile(fileName)
-            val layers = input.mapValues { ScanningLayer(range = it.value) }
-            return PacketScanner(currentLayerOfPacket = -1, layers = layers)
-        }
-
         /** compute the minimum delay necessary to pass through the firewall without getting caught.
          *    we return the first number that fulfills:
          *       for every scanner N: scanner_N is not at position 0 at timestamp (depth_N + delay)
@@ -65,52 +32,18 @@ data class PacketScanner(var currentLayerOfPacket: Int, val layers: Map<Int, Sca
             return generateSequence(seed = 0) { i -> i + 1 }
                     .find { i -> scanners.none { scanner -> (i + scanner.depth) % ((scanner.range - 1) * 2) == 0 } }!!
         }
-    }
-    
-    /** Move the scanners.
-     *    Each scanner moves one step forward; if it is at the minimum / maximum position, it changes its direction
-     */
-    fun moveScanners() {
-        for (scanner in layers.values) {
-            scanner.move()
+
+        fun severityOfViolationsForInputFile(fileName: String): Int {
+            val input = parseInputFile(fileName)
+            val scanners = input.map { ScannerRecord(depth = it.key, range = it.value) }
+            return scanners.filter { scanner -> scanner.depth % ((scanner.range-1)*2) == 0}
+                    .fold(initial = 0) {accu, scanner -> accu + scanner.depth * scanner.range}
         }
     }
 
-    /** Perform the next step
-     *
-     */
-    fun step(): Unit {
-        movePacket()
-        updateViolations()
-        moveScanners()
-    }
-
-    /** Solve the puzzle, and return the severity
-     *    The severity is the sum of the violation weights; the weight of each violation is its depth multiplied by its range
-     *
-     */
-    fun solve(): Int {
-        val maxLayer = layers.keys.max() ?: -1
-        while (currentLayerOfPacket <= maxLayer) {
-            step()
-        }
-        return violations.toList().fold(initial = 0) { accu, (depth,range) -> accu + depth * range }
-    }
-
-    private fun updateViolations() {
-        if (layers.contains(currentLayerOfPacket) && (layers.getValue(currentLayerOfPacket).scannerPosition == 0)) {
-            violations[currentLayerOfPacket] = layers.getValue(currentLayerOfPacket).range
-        }
-    }
-
-    private fun movePacket() {
-        currentLayerOfPacket += 1
-    }
 }
 
 
 fun main(args: Array<String>) {
-    val scanner = PacketScanner.getScannerForInputFile("input.txt")
-    println("part I: ${scanner.solve()}")
 }
 
--- 2017/13-packet_scanners/src/test/kotlin/de/qwhon/aoc/PacketScannerTest.kt
+++ 2017/13-packet_scanners/src/test/kotlin/de/qwhon/aoc/PacketScannerTest.kt
@@ -12,85 +12,14 @@ class PacketScannerTest {
     }
 
     @Test
-    fun testGetScannerForSampleInputShouldBeCorrect() {
-        val got = PacketScanner.getScannerForInputFile("sample_input.txt")
-        assertEquals("scanner for sample input",
-                PacketScanner(currentLayerOfPacket = -1,
-                        layers = hashMapOf(0 to ScanningLayer(3),
-                                1 to ScanningLayer(2),
-                                4 to ScanningLayer(4),
-                                6 to ScanningLayer(4))),
-                got)
-    }
-
-    @Test
-    fun testStepShouldIncreaseLayerOfPacket() {
-        val scanner = PacketScanner.getScannerForInputFile("sample_input.txt")
-        scanner.step()
-        assertEquals("Packet should now be in layer 0", 0, scanner.currentLayerOfPacket)
-    }
-
-    @Test
-    fun testStepShouldMoveScanners() {
-        val scanner = PacketScanner.getScannerForInputFile("sample_input.txt")
-        scanner.step()
-        assertEquals("should increase pos by 1 if direction = forward",
-                ScanningLayer(range = 3, scannerDirection = 1, scannerPosition = 1),
-                scanner.layers[0])
-        scanner.step()
-        assertEquals("should still increase pos by 1 if direction = forward",
-                ScanningLayer(range = 3, scannerDirection = 1, scannerPosition = 2),
-                scanner.layers[0])
-        scanner.step()
-        assertEquals("should change direction and decrease pos by 1 if at max",
-                ScanningLayer(range = 3, scannerDirection = -1, scannerPosition = 1),
-                scanner.layers[0])
-        scanner.step()
-        assertEquals("should still decrease pos by 1",
-                ScanningLayer(range = 3, scannerDirection = -1, scannerPosition = 0),
-                scanner.layers[0])
-        scanner.step()
-        assertEquals("should change direction and increase pos by 1 if at 0",
-                ScanningLayer(range = 3, scannerDirection = 1, scannerPosition = 1),
-                scanner.layers[0])
-    }
-
-    @Test
-    fun testStepShouldUpdateListOfViolations() {
-        val scanner = PacketScanner.getScannerForInputFile("sample_input.txt")
-        scanner.step()
-        assertEquals("first violation on step 1, since scanner on layer 0 is at pos 0",
-                hashMapOf(0 to 3), scanner.violations)
-        scanner.step()
-        assertEquals("no violation on step 2, since scanner on layer 1 is at pos 1",
-                hashMapOf(0 to 3), scanner.violations)
-        scanner.step()
-        assertEquals("no violation on step 3, since there is no scanner on layer 2",
-                hashMapOf(0 to 3), scanner.violations)
-        scanner.step()
-        assertEquals("no violation on step 3, since there is no scanner on layer 3",
-                hashMapOf(0 to 3), scanner.violations)
-        scanner.step()
-        assertEquals("no violation on step 4, since scanner on layer 4 is elsewhere",
-                hashMapOf(0 to 3), scanner.violations)
-        scanner.step()
-        assertEquals("no violation on step 3, since there is no scanner on layer 5",
-                hashMapOf(0 to 3), scanner.violations)
-        scanner.step()
-        assertEquals("second violation on step 6, since scanner on layer 6 is at pos 0",
-                hashMapOf(0 to 3, 6 to 4), scanner.violations)
-    }
-
-    @Test
     fun testSolveShouldReturnCorrectSolutionForExample() {
-        val scanner = PacketScanner.getScannerForInputFile("sample_input.txt")
-        assertEquals("severity for sample input: 0*3 + 6*4 = 24", 24, scanner.solve())
+        assertEquals("severity for sample input: 0*3 + 6*4 = 24", 24, PacketScanner.severityOfViolationsForInputFile("sample_input.txt"))
     }
 
     @Test
     fun testSolveShouldSolvePartI() {
-        val scanner = PacketScanner.getScannerForInputFile("input.txt")
-        assertEquals("severity for puzzle input", 1876, scanner.solve())
+        assertEquals("severity for puzzle input", 1876, PacketScanner.severityOfViolationsForInputFile("input.txt"))
+
     }
 
     // Tests for minimum delay
