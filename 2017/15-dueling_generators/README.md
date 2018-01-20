749eb8898ea54b530cc3edc911f77464431c8165
Frank Schmitt <frank@qwhon.de>
2018-01-20 23:12:47 +0100

    2017-15 dueling generators: more basic infrastructure for OCaml (OUnit, JBuilder, ..)

--- 2017/15-dueling_generators/README.md
+++ 2017/15-dueling_generators/README.md
@@ -6,9 +6,14 @@ Solution for the dueling generators puzzle, in OCaml.
 # Usage
 
 ## Installation
+opam install ounit
 
 ## Running the tests
-jbuilder test
+jbuilder build dueling_generators.exe
+_build/default/dueling_generators.exe
+
+OUTDATED :
+ocamlfind ocamlc -o dueling_generators -package core -package oUnit -linkpkg -g dueling_generators.ml
 
 ## Solving the puzzle
 
--- 2017/15-dueling_generators/dueling_generators.ml
+++ 2017/15-dueling_generators/dueling_generators.ml
@@ -1,5 +1,35 @@
-(*open Core.Std;;*)
+open Core;; 
+open Core.List ;;
+open OUnit;; 
 
+let unity x = x ;;
 
-print_endline "Hello, world!"
+(* for start values 65 and 8921:
+   1092455   430625591
+1181022009  1233683848
+ 245556042  1431495498
+1744312007   137874439
+1352636452   285222916
+ * *)
 
+let generatorA x = [1092455];;
+
+let testGeneratorA test_ctxt = assert_equal [1092455] (List.take (generatorA 65) 1);;  
+let test2 test_ctxt = assert_equal 100 (unity 100) ;;
+
+(* Name the test cases and group them together *)
+let suite =
+"suite">:::
+ ["test1">:: testGeneratorA;
+  "test2">:: test2]
+;;
+
+let _ = run_test_tt_main suite
+;;
+
+(*
+let%test _ = [1092455] == (List.take (generatorA 65) 1);;
+let%test _ = is_prime 7
+let%test _ = not (is_prime 1)
+let%test _ = not (is_prime 8)
+*)
