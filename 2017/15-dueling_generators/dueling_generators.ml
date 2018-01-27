d798d0fd90ff0cdd9e01de8ce288596835655a5b
Frank Schmitt <frank@qwhon.de>
2018-01-27 17:55:32 +0100

    2017-15 dueling generators: solved part I (brute force, 25 seconds)

--- 2017/15-dueling_generators/dueling_generators.ml
+++ 2017/15-dueling_generators/dueling_generators.ml
@@ -22,10 +22,11 @@ let sameLowest16 a b =
 
 (* judge: compute the number of equalities of genA and genB (lowest 16 bits) for num_rounds *) 
 let judge num_rounds genA genB = 
-  let lst_a = Batteries.LazyList.to_list(Batteries.LazyList.take num_rounds genA) in
-  let lst_b = Batteries.LazyList.to_list(Batteries.LazyList.take num_rounds genB) in
-  let zipped = List.zip_exn lst_a lst_b in
-  List.fold zipped ~init:0 ~f:(fun accu (a,b) -> if sameLowest16 a b then accu + 1 else accu);;
+  let lst_a = Batteries.LazyList.take num_rounds genA in
+  let lst_b = Batteries.LazyList.take num_rounds genB in
+  let zipped = Batteries.LazyList.combine lst_a lst_b in
+  (*List.fold zipped ~init:0 ~f:(fun accu (a,b) -> if sameLowest16 a b then accu + 1 else accu);;*)
+  Batteries.LazyList.fold_left (fun accu (a,b) -> if sameLowest16 a b then accu + 1 else accu) 0 zipped;; 
 
 (* UNIT TESTS *)
 let testGeneratorA test_ctxt = assert_equal 
@@ -41,12 +42,20 @@ let testJudge test_ctxt =
   let genB = generatorB 8921 in
     assert_equal 1 (judge 5 genA genB);;
 
+let testSolvePartI test_ctxt = 
+  let genA = generatorA 634 in
+  let genB = generatorB 301 in
+  let solution = judge 40000000 genA genB in
+  let _ = print_int solution in 
+  assert_equal 573 solution;; 
+
 (* unit tests *)
 let suite =
 "suite">:::
  ["generator A, first 5 values">:: testGeneratorA;
   "generator B, first 5 values">:: testGeneratorB;
-  "judge A B, first 5 rounds">:: testJudge]
+  "judge A B, first 5 rounds">:: testJudge;
+  "solve part I">:: testSolvePartI]
 ;;
 
 let _ = run_test_tt_main suite
