13b5942b0cb3b9d7f95abec13661e63053c112aa
Frank Schmitt <frank@qwhon.de>
2018-01-31 22:23:49 +0100

    2017-14 disk defragmenter: calling Clojure function from Java (kind of) works

--- 2017/14-disk_defragmenter/src/main/java/de/qwhon/aoc/DiskDefragmenter.java
+++ 2017/14-disk_defragmenter/src/main/java/de/qwhon/aoc/DiskDefragmenter.java
@@ -1,14 +1,28 @@
 package de.qwhon.aoc;
 
 import clojure.java.api.Clojure;
+import clojure.lang.IFn;
+//import knot_hash.core;
 
 public class DiskDefragmenter {
 
     public static void main(String[] args) {
-        // TODO: this doesn't work yet!
-      clojure.lang.IFn computeHash = Clojure.var("knot-hash.core", "compute-hash-part-II");
-      computeHash.invoke("xyz");
-      System.out.println("It works!");
+
+        // NB: our namespace knot-hash contains a hyphen.  This is translated to
+        // an underscore in the file path.  It can be tricky to remember when to use a
+        // hyphen and when to use an underscore, so maybe a better practice is to
+        // avoid hypens in namespaces
+        IFn require = Clojure.var("clojure.core","require");
+        require.invoke(Clojure.read("knot_hash.core"));
+
+        IFn computeHash = Clojure.var("knot-hash.core", "compute-hash-part-II");
+        computeHash.invoke("xyz");
+        // TODO: how can we get the result? We get back a knot_hash.core.KnotHashState
+        //       but Java doesn't know this type
+        //knot_hash.core.KnotHashState state = computeHash.invoke("xyz");
+        //clojure.lang.IFn plus = Clojure.var("clojure.core", "+");
+        //plus.invoke(1, 2);
+        System.out.println("It works!");
     }
   }
 
