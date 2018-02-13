247db30ca9dddb2931c4c581fa2de33856b54706
Frank Schmitt <frank@qwhon.de>
2018-02-13 11:12:50 +0100

    2017-14 disk defragmenter: cleanup

--- 2017/14-disk_defragmenter/src/main/java/de/qwhon/aoc/DiskDefragmenter.java
+++ 2017/14-disk_defragmenter/src/main/java/de/qwhon/aoc/DiskDefragmenter.java
@@ -6,14 +6,10 @@ import java.util.stream.*;
 
 import clojure.java.api.Clojure;
 import clojure.lang.IFn;
-//import clojure.lang.AFn;
-//import knot_hash.core;
-//import knot_hash.core.KnotHashState;
 
 public class DiskDefragmenter {
 
     IFn solve2_;
-    IFn computeHash_;
 
     public DiskDefragmenter() {
         // NB: our namespace knot-hash contains a hyphen.  This is translated to
@@ -23,19 +19,11 @@ public class DiskDefragmenter {
         IFn require = Clojure.var("clojure.core","require");
         require.invoke(Clojure.read("knot_hash.core"));
 
-        computeHash_ = Clojure.var("knot-hash.core", "compute-hash-part-II");
         solve2_ = Clojure.var("knot-hash.core", "solve2");
     }
 
     public static void main(String[] args) throws Exception {
         DiskDefragmenter defragmenter = new DiskDefragmenter();
-        //defragmenter.computeHash_.invoke("xyz");
-        // TODO: how can we get the result? We get back a knot_hash.core.KnotHashState
-        //       but Java doesn't know this type
-        //knot_hash.core.KnotHashState state = (knot_hash.core.KnotHashState) computeHash.invoke("xyz");
-
-        //String s = (String) defragmenter.solve2_.invoke("197,97,204,108,1,29,5,71,0,50,2,255,248,78,254,63");
-        //System.out.println("It works!" + s);
         Grid grid = defragmenter.gridFor("jzgqcdpd");
         System.out.println("part I: " + grid.getOccupiedCellCount()); // 8074
         System.out.println("part II: " + grid.getConnectedComponentCount()); // 1212
--- 2017/14-disk_defragmenter/src/main/java/de/qwhon/aoc/Grid.java
+++ 2017/14-disk_defragmenter/src/main/java/de/qwhon/aoc/Grid.java
@@ -5,11 +5,6 @@ import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.List;
-import java.util.ArrayList;
-import java.util.Set;
-import java.util.HashSet;
-import java.util.stream.*;
-import java.util.stream.Collectors;
 
 import org.jgrapht.*;
 import org.jgrapht.graph.*;
@@ -43,43 +38,47 @@ public class Grid {
      * @return Number of 1 bits in the binary notation of the grid contents
      */
     public int getOccupiedCellCount() {
-        // quick variant: count bits in hex strings
-        //return contents.stream().map(s -> Grid.countBitsInHexString(s)).reduce(0, (accu, val) -> accu + val);
-        // slow variant: build int matrix, count non-0 cells
-        buildGridAsIntMatrix();
-        int cnt = 0;
-        for (int[] row : matrix) {
-            for (int val : row) {
-                if (val != 0) {
-                    ++cnt;
-                }
-            }
-        }
-        return cnt;
+        // count bits in hex strings
+        return contents.stream().map(s -> Grid.countBitsInHexString(s)).reduce(0, (accu, val) -> accu + val);
     }
 
+    /**
+     * Print a grid row to a string.
+     * Cells are separated by a dot '.'
+     * @param row Grid row
+     * @param width Width for each cell, e.g. width = 4 -> print(1) = "0001"
+     * @return String representation of the grid row
+     */
     private String printRow(int[] row, int width) {
         //return Arrays.stream(row).reduce("",
         //                                 (accu, val) -> accu + ((Integer)val).toString(),
         //                                 (accu1, accu2) -> accu1 + accu2);
-        // this is stupid. Why is the Java8 stream API so broken?
+        // this is stupid. Why is the Java8 stream API so broken, i.e. does not allow reduce: Container<T> -> U ?
         String s = "";
         String format = "%0" + ((Integer) width).toString() + "d.";
         for (int i : row) {
-            //s += ((Integer) i).toString() + ".";
             s += String.format(format, i);
         }
         return s;
     }
 
+    /**
+     * Print a complete grid to a string.
+     * Rows are separated by newlines.
+     * @param width Width for each cell, @see printRow()
+     * @return String representation of the gird
+     */
     public String printGrid(int width) {
-        //return Arrays.stream(this.matrix).map(row -> Arrays.toString(row)).reduce("", (accu, val) -> accu + "\n" + val);
         return Arrays.stream(this.matrix)
-                //.map(row -> Arrays.stream(row).reduce("", (accu, val) -> accu + ((Integer)val).toString()))
                 .map(row -> printRow(row, width))
                 .reduce("", (accu, val) -> accu + val + "\n");
     }
 
+    /**
+     * Write the String representation of the grid to the given text file.
+     * @param fileName Name of output file
+     * @param width Width of each cell, @see printRow()
+     */
     public void writeToFile(String fileName, int width) {
         try (PrintWriter out = new PrintWriter(fileName)) {
             out.println(printGrid(width));
@@ -91,13 +90,13 @@ public class Grid {
     /**
      * Convert a hex string to its binary representation (as int array)
      *
-     * @param s
-     * @return
+     * @param s input string in Hex (uppercase, lowercase and mixedCase are all ok)
+     * @return binary representation of the string
      */
     public static int[] hexToBinary(String s) {
-        // this is stupid - Streams would have been much nicer, but I gave up on it
-        //   since it has too many restrictions, e.g. no reduce :: Stream<T> -> U -> fn: U, T -> U -> U, i.e. you
-        //   cannot reduce a Stream to a different type, and many more
+        // this is stupid - using Java8 streams would have been much nicer, but I gave up on it
+        //   since it has too many restrictions, e.g. no reduce :: Stream<T> -> U
+        //   (i.e. you cannot reduce a Stream to a different type without also specifying a (useless) combiner function)
         StringBuilder res = new StringBuilder();
         for (char ch : s.toUpperCase().toCharArray()) {
             switch (ch) {
@@ -150,15 +149,13 @@ public class Grid {
                     res.append("1111");
                     break;
                 default:
-                    // stupidly return nonsense, since Java is too brain-dead to recognize its exception handlers
+                    // stupidly return nonsense, since apparently Java is too brain-dead to recognize an exception thrown inside
+                    // map() correctly
                     res.append("XXXX");
                     break;
             }
         }
-        //return res.toString().chars()
-        //        .map(c -> if (c == '0') { 0 } else { 1 })
-        //           .toArray(int[][]::new);
-        // 48: ASCII code of '0'
+        // 48: ASCII code of '0', i.e. map '0' -> 0, '1' -> 1
         return res.toString().chars().map(c -> c - 48).toArray();
     }
 
@@ -166,85 +163,37 @@ public class Grid {
      * Convert our grid contents from a list of hex strings to a 2D-Array of ints
      */
     private void buildGridAsIntMatrix() {
-        // this is the most stupid language restriction ever - I declare that the function may throw an Exception,
-        //   but the Java compiler still complains? WTF?
         this.matrix = contents.stream()
                 // convert each hex string to a binary string
                 .map(s -> hexToBinary(s)
                 )
-                //.map(s -> s.toArray())
                 .toArray(int[][]::new);
     }
 
-    private Integer[] getNeighbourComponents(int[][] matrix, int row, int col) {
-        Set<Integer> result = new HashSet<Integer>();
-        if (row > 0) {
-            // not topmost row? add neighbour at top if it isn't zero
-            if (matrix[row - 1][col] != 0) {
-                result.add(matrix[row - 1][col]);
-            }
-        }
-        // not leftmost col? add neighbour at left if it isn't zero
-        if (col > 0) {
-            if (matrix[row][col - 1] != 0) {
-                result.add(matrix[row][col - 1]);
-            }
-        }
-        // no need to check right / bottom neighbour - these weren't visited yet
-        // sort the components to ensure components with lower indices come first
-        Integer[] tmp = result.toArray(new Integer[0]);
-        Arrays.sort(tmp);
-        return tmp;
-    }
-
     /**
-     * Renumber the grid cells (merge two distinct areas)
-     *
-     * @param neighbourComponents Grid areas we want to merge (#1 gets renumbered to #0)
-     * @param uptoRow             we renumber all grid rows from 0 .. uptoRow
-     * @param uptoCol             we renumber all grid cols from 0 .. uptoCol
+     * Get a unique label for the given node
+     * @param row row of the node (in the grid)
+     * @param col column of the node (in the grid)
+     * @return unique label
      */
-    private void renumberGridCells(Integer[] neighbourComponents, int uptoRow, int uptoCol) {
-        for (int i = 0; i <= uptoRow; ++i) {
-            for (int j = 0; j <= uptoCol; ++j) {
-                if (this.matrix[i][j] == neighbourComponents[1]) {
-                    this.matrix[i][j] = neighbourComponents[0];
-                }
-            }
-        }
-    }
-
     private int nodeLabel(int row, int col) {
+        // HACK we rely on the fact that our grid is never larger than 999x999 cells
         return row*1000 + col;
     }
 
+    /**
+     * Create an undirected graph representing our grid.
+     * For each occupied cell, we add a node, and for each connection between occupied neighbour cells,
+     * we add an edge.
+     * @return the result graph
+     */
     private Graph<Integer, DefaultEdge> createGraph()
     {
         Graph<Integer, DefaultEdge> g = new SimpleGraph<>(DefaultEdge.class);
 
-        /*String v1 = "v1";
-        String v2 = "v2";
-        String v3 = "v3";
-        String v4 = "v4";
-
-        // add the vertices
-        g.addVertex(v1);
-        g.addVertex(v2);
-        g.addVertex(v3);
-        g.addVertex(v4);
-
-        // add edges to create a circuit
-        g.addEdge(v1, v2);
-        g.addEdge(v2, v3);
-        g.addEdge(v3, v4);
-        g.addEdge(v4, v1);
-*/
         for (int i = 0; i < this.matrix.length; ++i) {
             int[] row = this.matrix[i];
             for (int j = 0; j < row.length; ++j) {
-                //if (this.matrix[i][j] == neighbourComponents[1]) {
-                //    this.matrix[i][j] = neighbourComponents[0];
-                //}
                 // Ignore 0 cells
                 if (row[j] != 0) {
                     // add node
@@ -270,66 +219,10 @@ public class Grid {
      * @return the number of connected components
      */
     public int getConnectedComponentCount() {
-        //List<String> contentsInBinary = contents.stream().map(s -> new BigInteger(s, 16).toString(2)).collect(Collectors.toList());
         buildGridAsIntMatrix();
-        //System.out.println("matrix before: " + printGrid(matrix));
-        /*buildConnectedComponents();
-
-
-        //System.out.println("matrix after: " + printGrid(matrix));
-        // count the disjunct areas
-        Set<Integer> result = countConnectedComponents();
-        return result.size();
-        */
         Graph<Integer, DefaultEdge> g = createGraph();
         ConnectivityInspector<Integer, DefaultEdge> inspector = new ConnectivityInspector<>(g);
         return inspector.connectedSets().size();
-        //return cnt;
-    }
-
-    private Set<Integer> countConnectedComponents() {
-        Set<Integer> result = new HashSet<Integer>();
-        for (int[] row : matrix) {
-            for (int val : row) {
-                if (val != 0) {
-                    result.add(val);
-                }
-            }
-        }
-        return result;
-    }
-
-    private void buildConnectedComponents() {
-        //Set<Integer> components = new Set<Integer();
-        // first index used for renumbering (we don't use 1, since 1 marks the occupied cells we don't have visited yet)
-        int currIndex = 2;
-        for (int i = 0; i < matrix.length; ++i) {
-            int[] row = matrix[i];
-            for (int j = 0; j < row.length; ++j) {
-                // ignore 0 cells
-                if (row[j] != 0) {
-                    Integer[] neighbourComponents = getNeighbourComponents(matrix, i, j);
-                    // no neighbour set? use current index + increase it
-                    if (neighbourComponents.length == 0) {
-                        row[j] = currIndex++;
-                        //if (i == 127) writeToFile(String.format("debug/sample-%03d-%03d_standalone.txt", i, j), 4);
-                    }
-                    // one neighbour component: use its component
-                    else if (neighbourComponents.length == 1) {
-                        row[j] = neighbourComponents[0];
-                        //if (i == 127) writeToFile(String.format("debug/sample-%03d-%03d_neighbour-%03d.txt", i, j,
-                        //        neighbourComponents[0]), 4);
-                    }
-                    // two neighbour components: use the first component, and re-number all neighbours to use this component
-                    else {
-                        row[j] = neighbourComponents[0];
-                        renumberGridCells(neighbourComponents, i, j);
-                        //if (i == 127) writeToFile(String.format("debug/sample-%03d-%03d_merged-%03d-%03d.txt", i, j,
-                        //        neighbourComponents[0], neighbourComponents[1]), 4);
-                    }
-                }
-            }
-        }
     }
 
     /**
