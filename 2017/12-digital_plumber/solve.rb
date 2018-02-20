6fd1922274f9d29ba7e752927dabacd686079f6d
Frank Schmitt <frank@qwhon.de>
2018-02-20 21:24:19 +0100

    2017-12 digital plumber: use rubocop to ensure coding style is kept

--- 2017/12-digital_plumber/solve.rb
+++ 2017/12-digital_plumber/solve.rb
@@ -1,5 +1,5 @@
 class DigitalPlumber
-  attr_reader :edges 
+  attr_reader :edges
 
   def initialize(input)
     parse_input(input)
@@ -7,18 +7,18 @@ class DigitalPlumber
 
   def parse_input(input)
     @edges = []
-    re = /^([0-9]+) <-> ([0-9, ]+)/ 
+    re = /^([0-9]+) <-> ([0-9, ]+)/
     input.each do |line|
       md = re.match(line)
       node = md[1].to_i
-      neighbours = md[2].split(",").map {|s| s.to_i}
-      # we store edges and - for easier lookup always use the lower index first
+      neighbours = md[2].split(',').map(&:to_i)
+      # we store edges and (for easier lookup) always use the lower index first
       neighbours.each do |n|
         v1 = [node, n].min
         v2 = [node, n].max
         @edges.push([v1, v2])
       end
-    end 
+    end
     @edges.uniq!
   end
 
@@ -26,8 +26,8 @@ class DigitalPlumber
   def initialize_groups
     @groups = []
     @edges.each do |e|
-      @groups.push([e[0]]) # single-element group 
-      @groups.push([e[1]]) # single-element group 
+      @groups.push([e[0]]) # single-element group
+      @groups.push([e[1]]) # single-element group
     end
     @groups.uniq!
   end
@@ -36,19 +36,20 @@ class DigitalPlumber
   # removes both groups from the list, and adds a new group which is the
   # merger of the groups
   def merge_groups(g1, g2)
-    #puts "merging #{g1} and #{g2}"
+    # puts 'merging #{g1} and #{g2}'
     g = (g1 + g2).uniq # use uniq to handle the case when g1 == g2
     @groups.delete g1
     @groups.delete g2
     @groups.push g
-    #puts "merged to #{g}"
+    # puts 'merged to #{g}'
   end
 
   # find all groups in the graph
   def find_groups
     initialize_groups
     @edges.each do |e|
-      v1, v2 = e[0], e[1]
+      v1 = e[0]
+      v2 = e[1]
       g1 = group_for_id(v1)
       g2 = group_for_id(v2)
       merge_groups(g1, g2)
@@ -56,14 +57,14 @@ class DigitalPlumber
   end
 
   # get the group containing the given vertex
-  def group_for_id(id) 
-    find_groups if @groups == nil
-    @groups.detect {|g| g.include? id }
+  def group_for_id(id)
+    find_groups if @groups.nil?
+    @groups.detect { |g| g.include? id }
   end
 
   # get a list of all groups
   def groups
-    find_groups if @groups == nil
+    find_groups if @groups.nil?
     @groups
   end
 end
@@ -71,17 +72,18 @@ end
 #####################
 # TESTS             #
 #####################
-describe "Digital Plumber" do
-  it "builds a list of edges from single-element input" do
-    conns = [ "1 <-> 1"]
+describe 'Digital Plumber' do
+  it 'builds a list of edges from single-element input' do
+    conns = ['1 <-> 1']
     dp = DigitalPlumber.new(conns)
     expect(dp.edges).to eq([[1, 1]])
   end
 
-  it "builds a list of edges for the sample input" do
-    conns = IO.readlines("sample_input.txt")
+  it 'builds a list of edges for the sample input' do
+    conns = IO.readlines('sample_input.txt')
     dp = DigitalPlumber.new(conns)
-    expect(dp.edges).to eq([
+    expect(dp.edges).to eq(
+    [
       [0, 2],
       [1, 1],
       [2, 3],
@@ -92,26 +94,26 @@ describe "Digital Plumber" do
     ])
   end
 
-  it "returns a one-element group for a single input" do
-    conns = [ "1 <-> 1"]
+  it 'returns a one-element group for a single input' do
+    conns = ['1 <-> 1']
     dp = DigitalPlumber.new(conns)
     expect(dp.group_for_id(1)).to eq([1])
   end
 
-  it "finds the groups for the sample input" do
-    conns = IO.readlines("sample_input.txt")
+  it 'finds the groups for the sample input' do
+    conns = IO.readlines('sample_input.txt')
     dp = DigitalPlumber.new(conns)
     expect(dp.group_for_id(0).count).to eq(6)
   end
 
-  it "solves part I" do
-    conns = IO.readlines("input.txt")
+  it 'solves part I' do
+    conns = IO.readlines('input.txt')
     dp = DigitalPlumber.new(conns)
     expect(dp.group_for_id(0).count).to eq(306)
   end
 
-  it "solves part II" do
-    conns = IO.readlines("input.txt")
+  it 'solves part II' do
+    conns = IO.readlines('input.txt')
     dp = DigitalPlumber.new(conns)
     expect(dp.groups.count).to eq(200)
   end
