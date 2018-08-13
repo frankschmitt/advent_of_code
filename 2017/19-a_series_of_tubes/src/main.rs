3ea6305919278e7101ac6490a3094e5288a6cf84
Frank Schmitt <frank@qwhon.de>
2018-08-13 23:13:37 +0200

    2017-19 started parsing of input (BROKEN DUE TO TYPE ERROR)

--- 2017/19-a_series_of_tubes/src/main.rs
+++ 2017/19-a_series_of_tubes/src/main.rs
@@ -5,17 +5,43 @@ mod tests {
   use std::fs::File;
   use std::path::Path;
   use std::io::{self, BufRead};
+  use std::char;
     
-  enum Direction {
-    North,
-    East,
-    South,
-    West
+  #[derive(Debug, PartialEq)]
+  enum CellType {
+    Empty,
+    Vertical,
+    Horizontal,
+    Corner,
+    Letter { letter: char }
   }
 
+  #[derive(Debug, Clone)]
   struct Grid {
     num_rows: i32,
-    num_cols: i32
+    num_cols: i32,
+    cells: Vec<String>
+  }
+
+  impl Grid {
+
+    fn cell(&self, row: i32, col: i32) -> CellType {
+      if row >= self.num_rows || col >= self.num_cols {
+        return CellType::Empty;
+      }
+      let line = &self.cells[row as usize];
+      let byte = &line.into_bytes()[col as usize];
+      match char::from_u32(*byte as u32) { 
+        Some(ch) => match ch {
+          ' ' => CellType::Empty,
+          '|' => CellType::Vertical,
+          '-' => CellType::Horizontal,
+          '+' => CellType::Corner,
+          c   => CellType::Letter { letter: c }
+       },
+       None => CellType::Empty
+      }
+    }
   }
 
   fn read_grid_from_file<P>(filename: P) -> Result<Grid, io::Error>  
@@ -23,16 +49,17 @@ mod tests {
   {
     let file = try!(File::open(filename));
     let lines = io::BufReader::new(file).lines();
-    let mut vec = Vec::new();
+    let mut cells = Vec::new();
     for line in lines {
       match line {
-        Ok(s) => vec.push(s),
+        Ok(s) => cells.push(s),
         Err(_) => println!("error!"), // ignore errors for now
       }
     }
     // let input = fs::read_to_string(filename).expect("Unable to read file");
-    
-    let grid = Grid { num_rows: vec.len() as i32, num_cols: vec[0].len() as i32 };
+    let grid = Grid { num_rows: cells.len() as i32, 
+                      num_cols: cells[0].len() as i32,
+                      cells: cells };
     return Ok(grid);
   }
 
@@ -43,6 +70,9 @@ mod tests {
       Ok(grid) => { 
                     assert_eq!(7, grid.num_rows);
                     assert_eq!(16, grid.num_cols);
+                    assert_eq!(CellType::Vertical, grid.cell(0, 5));
+                    assert_eq!(CellType::Vertical, grid.cell(1, 5));
+                    assert_eq!(CellType::Letter { letter: 'A' }, grid.cell(2, 5));
                   },
       Err(_) => assert!(false),
     }
