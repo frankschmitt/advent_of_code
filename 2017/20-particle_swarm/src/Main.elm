fc6f92ecef49ea1650ab3b5591a27a21a13c7bf8
Frank Schmitt <frank@qwhon.de>
2018-09-30 22:45:14 +0200

    2017-20 step for particles

--- 2017/20-particle_swarm/src/Main.elm
+++ 2017/20-particle_swarm/src/Main.elm
@@ -14,9 +14,49 @@ type alias Vector3D =
     }
 
 
+type alias Particle =
+    { index : Int
+    , position : Vector3D
+    , velocity : Vector3D
+    , acceleration : Vector3D
+    }
+
+
+
+-- move the given 3D vector by the given amount (using vector addition)
+
+
 move : Vector3D -> Vector3D -> Vector3D
 move v1 v2 =
     { x = v1.x + v2.x, y = v1.y + v2.y, z = v1.z + v2.z }
+
+
+step : Particle -> Particle
+step { index, position, velocity, acceleration } =
+    let
+        v_x =
+            velocity.x + acceleration.x
+
+        v_y =
+            velocity.y + acceleration.y
+
+        v_z =
+            velocity.z + acceleration.z
+
+        x =
+            position.x + v_x
+
+        y =
+            position.y + v_y
+
+        z =
+            position.z + v_z
+    in
+    { index = index
+    , position = { x = x, y = y, z = z }
+    , velocity = { x = v_x, y = v_y, z = v_z }
+    , acceleration = acceleration
+    }
 
 
 type alias Model =
--- 2017/20-particle_swarm/tests/Tests.elm
+++ 2017/20-particle_swarm/tests/Tests.elm
@@ -20,4 +20,8 @@ all =
         , test "adding a vector with v(1,2,3) should increase it by x=1, y=2, z=3" <|
             \_ ->
                 Expect.equal { x = 5, y = 7, z = 9 } (Main.move { x = 1, y = 2, z = 3 } { x = 4, y = 5, z = 6 })
+        , test "moving a particle should update its position and velocity" <|
+            \_ ->
+                Expect.equal { index = 1, position = { x = 4, y = 0, z = 0 }, velocity = { x = 1, y = 0, z = 0 }, acceleration = { x = -1, y = 0, z = 0 } }
+                    (Main.step { index = 1, position = { x = 3, y = 0, z = 0 }, velocity = { x = 2, y = 0, z = 0 }, acceleration = { x = -1, y = 0, z = 0 } })
         ]
