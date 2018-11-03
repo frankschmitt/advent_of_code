d6546f293e0b7335c65794a3cdfc8d2bed589490
Frank Schmitt <frank@qwhon.de>
2018-11-03 11:47:04 +0100

    2017-21 fractal art: basic rule + grid parsing + checking for matches

--- 2017/21-fractal_art/fractalart/R/fractalart.R
+++ 2017/21-fractal_art/fractalart/R/fractalart.R
@@ -1,5 +1,24 @@
-# placeholder 
+library(rlang)
 
 initial_grid = function() {
   '.#.\n..#\n###' 
+}
+
+# Check whether the given rule matches the given grid
+# The approach is pretty straightforward:
+# - check whether the lengths are compatible (i.e. an even-length rule might match only an even-length grid)
+# - TODO check all rotations of the pattern against the grid
+matches = function(rule, grid) {
+  (length(as_bytes(rule[1])) == length(as_bytes(grid[1]))) &&
+    (rule[1] == grid[1]) 
+}
+
+# Parse a rule from a string.
+# We split the string on => and return the result as a simple 2-element vector (input, output)
+rule = function(input) {
+  unlist(strsplit(input, " => "))
+}
+
+grid = function(input) {
+  input
 }
--- 2017/21-fractal_art/fractalart/tests/testthat/testFractalArt.R
+++ 2017/21-fractal_art/fractalart/tests/testthat/testFractalArt.R
@@ -3,5 +3,26 @@
 context("fractalart")
 
 test_that("sets the initial grid", {
-  expect_equal('.#.\n..#\n###', initial_grid())
+  expect_equal(".#.\n..#\n###", initial_grid())
+})
+
+test_that("constructing a 2x2 grid", {
+  expect_equal("../.#", grid("../.#"))
+})
+
+test_that("constructing a rule for 2x2", {
+  expect_equal(c("../.#", "##./#../..."), rule("../.# => ##./#../..."))
+})
+
+test_that("2x2 rule should match a 2x2 grid which is an exact match", {
+  expect_equal(TRUE, matches(rule("../.# => ##./#../..."), grid("../.#")))
+})
+
+test_that("2x2 rule should not match a 2x2 grid which does not match", {
+  expect_equal(FALSE, matches(rule("../.# => ##./#../..."), grid("../..")))
+})
+
+
+test_that("2x2 rule should not match 3x3 grid whose 2x2 subgrid is an exact match", {
+  expect_equal(FALSE, matches(rule("../.# => ##./#../..."), grid(".../.#./...")))
 })
