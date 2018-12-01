9ed5ececfa3afd7e2c410976d3c330f1a3be3b79
Frank Schmitt <frank@qwhon.de>
2018-12-01 15:10:28 +0100

    2018-01 chronal calibration: solved part II

--- 2018/01-chronal_calibration/Solve.hs
+++ 2018/01-chronal_calibration/Solve.hs
@@ -1,5 +1,7 @@
 module Solve where
 
+import qualified Data.Set as Set
+
 frequency :: [Int] -> Int
 frequency = foldl (+) 0 
 
@@ -15,12 +17,25 @@ parseInput input =
 solveI :: String -> Int
 solveI input = frequency $ parseInput input
 
-solve = undefined
+-- recursive helper for part II
+-- arguments: list of frequency diffs, frequencies seen so far, current frequency
+-- returns: first frequency that repeats
+solveII' :: [Int] -> Set.Set Int -> Int -> Int 
+solveII' (x:xs) seen currentFreq =
+  let 
+    nextFreq = currentFreq + x
+  in 
+    if Set.member nextFreq seen then nextFreq
+    else solveII' xs (Set.insert nextFreq seen) nextFreq
 
+solveII :: String -> Int 
+solveII input = solveII' repeatFrequencies Set.empty 0  
+  where repeatFrequencies = cycle $ parseInput input
 
 #if defined(STANDALONE)
 main = do
     input <- readFile "input.txt"
-    putStrLn $ show $ solveI input  -- (16021, 7685)
+    putStrLn $ show $ solveI  input  
+    putStrLn $ show $ solveII input  
 #endif
 
--- 2018/01-chronal_calibration/_Solve.hs
+++ 2018/01-chronal_calibration/_Solve.hs
@@ -4,11 +4,12 @@ import Data.List
 import Test.HUnit
 import Solve
 
-testSolvePartI  = test [ "solve for sample input" ~: 2 ~=? solveI "+1\n-2\n+3\n"
-                       , "parses input" ~: [1,-2,3]    ~=? parseInput "+1\n-2\n+3\n"
-                       ]
+testSolve = test [ "solveI for sample input" ~: 10 ~=? solveII "+3\n+3\n+4\n-2\n-4\n"
+                 , "solveII for sample input" ~: 2 ~=? solveI  "+1\n-2\n+3\n"
+                 , "parses input" ~: [1,-2,3]    ~=? parseInput "+1\n-2\n+3\n"
+                 ]
 
-tests = TestList [ testSolvePartI
+tests = TestList [ testSolve
                  ] 
 
 main = do runTestTT tests
