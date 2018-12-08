e3ee2cfacffb0c8eb68a47ccd47202cdf30a2d85
Frank Schmitt <frank@qwhon.de>
2018-12-08 09:03:38 +0100

    2018-05 alchemical reduction: cleanup

--- 2018/05-alchemical_reduction/Solve.hs
+++ 2018/05-alchemical_reduction/Solve.hs
@@ -8,9 +8,6 @@ reduce :: String -> String
 reduce inp = case inp of
   []  -> [] 
   [x] -> if x == '\n' then [] else [x] -- omit trailing newline
-  --x : y : ys -> if x == y then [x] ++ (reduce (y : ys)) -- do not reduce same characters
-  --              else if toUpper(x) == toUpper(y) then reduce ys -- reduce pair
-  --              else [x] ++ reduce (y : ys)    
   x : y : ys -> if ((x /= y) && (toUpper(x) == toUpper(y))) then reduce ys -- reduce pair
                 else [x] ++ reduce (y : ys)    
 
@@ -22,19 +19,13 @@ fullyReduce input =
   -- else DT.trace ("fullyReduce, len: " ++ show (length reduced)) (fullyReduce(reduced))
   else fullyReduce reduced
 
-removeAllPairs :: String -> Char -> String
-removeAllPairs inp ch = case inp of
-  []  -> [] 
-  [x] -> if x == '\n' then [] else [x] -- omit trailing newline
-  x : y : ys -> if (toUpper(x) == toUpper(y) && toUpper(x) == ch) then removeAllPairs ys ch
-                else [x] ++ removeAllPairs (y : ys) ch
-
 removeAll :: String -> Char -> String
 removeAll s c = filter (\c' -> c' /= c && c' /= toUpper(c)) s
 
 solveI :: String -> Int 
 solveI = length . fullyReduce
 
+solveII :: String -> Int
 solveII input = minimum $ map (\ch -> length $ fullyReduce $ removeAll input ch) ['a' .. 'z']
 
 #if defined(STANDALONE)
