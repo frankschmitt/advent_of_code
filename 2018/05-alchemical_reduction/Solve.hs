70aa87f2059dd9194794b826f4ea035052fc4ae4
Frank Schmitt <frank@qwhon.de>
2018-12-08 09:02:19 +0100

    2018-05 alchemical reduction: solved part II

--- 2018/05-alchemical_reduction/Solve.hs
+++ 2018/05-alchemical_reduction/Solve.hs
@@ -2,14 +2,16 @@ module Solve where
 
 import Data.Char
 import qualified Debug.Trace as DT
-
+import Data.List
 
 reduce :: String -> String
 reduce inp = case inp of
   []  -> [] 
   [x] -> if x == '\n' then [] else [x] -- omit trailing newline
-  x : y : ys -> if x == y then [x] ++ (reduce (y : ys)) -- do not reduce same characters
-                else if toUpper(x) == toUpper(y) then reduce ys -- reduce pair
+  --x : y : ys -> if x == y then [x] ++ (reduce (y : ys)) -- do not reduce same characters
+  --              else if toUpper(x) == toUpper(y) then reduce ys -- reduce pair
+  --              else [x] ++ reduce (y : ys)    
+  x : y : ys -> if ((x /= y) && (toUpper(x) == toUpper(y))) then reduce ys -- reduce pair
                 else [x] ++ reduce (y : ys)    
 
 fullyReduce :: String -> String
@@ -20,16 +22,24 @@ fullyReduce input =
   -- else DT.trace ("fullyReduce, len: " ++ show (length reduced)) (fullyReduce(reduced))
   else fullyReduce reduced
 
+removeAllPairs :: String -> Char -> String
+removeAllPairs inp ch = case inp of
+  []  -> [] 
+  [x] -> if x == '\n' then [] else [x] -- omit trailing newline
+  x : y : ys -> if (toUpper(x) == toUpper(y) && toUpper(x) == ch) then removeAllPairs ys ch
+                else [x] ++ removeAllPairs (y : ys) ch
+
+removeAll :: String -> Char -> String
+removeAll s c = filter (\c' -> c' /= c && c' /= toUpper(c)) s
+
 solveI :: String -> Int 
--- solveI input = (length $ DT.trace ("fullyReduced: >>" ++ s ++ "<<") s) - 1
 solveI = length . fullyReduce
 
-solveII = undefined
+solveII input = minimum $ map (\ch -> length $ fullyReduce $ removeAll input ch) ['a' .. 'z']
 
 #if defined(STANDALONE)
 main = do
     input <- readFile "input.txt"
     putStrLn $ show $ solveI  input 
---    putStrLn $ show $ solveII input 
+    putStrLn $ show $ solveII input 
 #endif
-
--- 2018/05-alchemical_reduction/_Solve.hs
+++ 2018/05-alchemical_reduction/_Solve.hs
@@ -7,7 +7,7 @@ import Solve
 sampleInput = "dabAcCaCBAcCcaDA"
 
 testSolveThem  = test [ "solves part I" ~: 10 ~=? solveI(sampleInput)
-                      , "solves part II" ~: 1 ~=? solveII
+                      , "solves part II" ~: 4 ~=? solveII(sampleInput)
                       ]
 
 tests = TestList [ testSolveThem
