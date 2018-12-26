5a5ffbb385e2980cf56560e31fd43015071cc9b1
Frank Schmitt <frank@qwhon.de>
2018-12-26 22:05:46 +0100

    2018-07 the sum of its parts: first draft of part I (in Haskell)

--- 2018/07-the_sum_of_its_parts/Solve.hs
+++ 2018/07-the_sum_of_its_parts/Solve.hs
@@ -1,7 +1,52 @@
 module Solve where
 
+import Data.List
 
-solveI = undefined
+type Node = String
+type Edge = (String,String)
+
+{-
+data Graph = MkGraph {
+  nodes :: [Node],
+  edges :: [Edge]
+} 
+-}
+type Graph = ([Node], [Edge])
+
+-- parse the input, and return a list of edges
+parseInput :: String -> [Edge]
+parseInput input = map makeEdge (lines input)
+  where makeEdge line = (start line, end line)
+        start line = (words line) !! 1
+        end line   = (words line) !! 7 
+
+-- given a list of edges, extract the nodes, and build the DAG
+buildGraph :: [Edge] -> Graph
+buildGraph edges = 
+  let 
+    startNodes = map (\(s,e) -> s) edges
+    endNodes   = map (\(s,e) -> e) edges
+    nodes = nub $ union startNodes endNodes 
+  in
+    (nodes,edges)
+
+-- find the path in the given graph
+findPath :: Graph -> [Node]
+findPath (nodes, edges) = 
+  case (nodes,edges) of 
+    ([],_) -> []      -- no nodes left -> return empty list
+    (nodes', []) -> sort nodes' -- no edges left -> sort the remaining nodes, and return them
+    (nodes', edges') -> [firstNode] ++ findPath (reducedNodes, reducedEdges)
+      where firstNode = head $ sort $ candidateNodes
+            reducedNodes = nodes' \\ [firstNode]
+            reducedEdges = filter (\(n1,n2) -> n1 /= firstNode && n2 /= firstNode) edges'
+            candidateNodes = filter hasNoIncomingEdges nodes'
+            hasNoIncomingEdges n = all (\(_,end) ->  n /= end) edges'
+
+
+solveI :: String -> String
+solveI input = concat $ findPath graph
+  where graph = buildGraph $ (parseInput input)
 
 solveII = undefined
 
@@ -10,6 +55,6 @@ solveII = undefined
 main = do
     input <- readFile "input.txt"
     putStrLn $ show $ solveI  input 
-    putStrLn $ show $ solveII input 
+    --putStrLn $ show $ solveII input 
 #endif
 
--- 2018/07-the_sum_of_its_parts/_Solve.hs
+++ 2018/07-the_sum_of_its_parts/_Solve.hs
@@ -4,7 +4,18 @@ import Data.List
 import Test.HUnit
 import Solve
 
-testSolveThem  = test [ "solves part I" ~: 1 ~=? solveI
+-- we changed the ordering of the instructions since the default ordering matches the solution and is therefore not a good test case
+sampleInput = unlines [
+  "Step A must be finished before step B can begin.",
+  "Step A must be finished before step D can begin.",
+  "Step C must be finished before step A can begin.",
+  "Step C must be finished before step F can begin.",
+  "Step B must be finished before step E can begin.",
+  "Step D must be finished before step E can begin.",
+  "Step F must be finished before step E can begin."
+  ]
+
+testSolveThem  = test [ "solves part I" ~: "CABDFE" ~=? (solveI sampleInput)
                       , "solves part II" ~: 1 ~=? solveII
                       ]
 
