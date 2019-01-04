692e524e70cbadafbc13284f0beda663e3dfc599
Frank Schmitt <frank@qwhon.de>
2019-01-04 20:14:34 +0100

    2018-07 the sum of its parts: reimplemented part I with Data.Graph (WRONG - does not impose correct ordering of nodes)

--- 2018/07-the_sum_of_its_parts/Solve.hs
+++ 2018/07-the_sum_of_its_parts/Solve.hs
@@ -2,138 +2,74 @@ module Solve where
 
 import Data.List
 import Data.Char
+import qualified Data.Graph as G
+import qualified Data.Graph.Inductive as GI
 import qualified Debug.Trace as DT
 
 type NodeLabel = String
--- a node is defined by its label and its delay
-type Node = (NodeLabel, Int)
+type NodeRecord = (NodeLabel, Int)
 -- an edge is defined by the labels of the nodes it connects
-type Edge = (NodeLabel, NodeLabel)
-
-type Second = Int
-type WorkerID = Int
-
--- a worker is either idle, or has been working on a task for N seconds, or has finished working
-data WorkerState = Idle | Working Node Second | Done Node
-  deriving (Show, Eq)
--- a worker is defined by their id and their state
-type Worker = (WorkerID, WorkerState) 
-
--- overall system state: we need to keep track of the graph, the workers and the current time
-type SystemState = (Graph, [Worker], Second)
-
-{-
-data Graph = MkGraph {
-  nodes :: [Node],
-  edges :: [Edge]
-} 
--}
-type Graph = ([Node], [Edge])
+type EdgeRecord = (NodeLabel, NodeLabel)
 
 -- parse the input, and return a list of edges
-parseInput :: String -> [Edge]
+parseInput :: String -> [EdgeRecord]
 parseInput input = map makeEdge (lines input)
   where makeEdge line = (start line, end line)
         start line = (words line) !! 1
         end line   = (words line) !! 7 
 
--- given a list of edges and an offset, extract the nodes, and build the DAG
-buildGraph :: [Edge] -> Int -> Graph
-buildGraph edges offset = 
+nodeLabelToNode :: NodeLabel -> NodeRecord
+nodeLabelToNode lbl = (lbl, (ord $ head lbl) - (ord 'A') + 1)
+
+nodeLabelsToNodes :: [NodeLabel] -> [NodeRecord]
+nodeLabelsToNodes nodeLabels = map nodeLabelToNode nodeLabels 
+
+edgesToNodeLabels :: [EdgeRecord] -> [NodeLabel]
+edgesToNodeLabels edges =  
   let 
     startNodeLabels = map (\(s,e) -> s) edges
     endNodeLabels   = map (\(s,e) -> e) edges
-    nodeLabels = nub $ union startNodeLabels endNodeLabels
-    nodes = map (\lbl -> (lbl, offset + (ord $ head lbl) - (ord 'A') + 1)) nodeLabels
   in
-    (nodes,edges)
+    sort $ nub $ union startNodeLabels endNodeLabels
 
--- remove a node from a graph, and return the reduced graph
-removeNode :: Graph -> Node -> Graph
-removeNode (nodes, edges) n = (reducedNodes, reducedEdges)
-  where reducedNodes = nodes \\ [n]
-        reducedEdges = filter (\(n1,n2) -> n1 /= (fst n) && n2 /= (fst n)) edges
 
--- find candidate nodes (those without incoming edges)
-findCandidateNodes :: Graph -> [Node]
-findCandidateNodes (nodes, edges) = filter hasNoIncomingEdges nodes
-  where hasNoIncomingEdges n = all (\(_,end) ->  (fst n) /= end) edges
+-- given a list of edges and an offset, extract the nodes, and build the DAG
+-- buildGraph :: [Edge] -> Graph
+buildGraph :: [EdgeRecord] -> (G.Graph, G.Vertex -> (String, Int, [Int]), Int -> Maybe G.Vertex)
+buildGraph edges = 
+  let 
+    nodeLabels = edgesToNodeLabels edges 
+    nodes = nodeLabelsToNodes nodeLabels 
+    neighbourLabels nodeLabel = map (\n -> snd n) $ filter (\(start, end) -> start == nodeLabel) edges
+    neighbours nodeLabel = map snd $ map nodeLabelToNode $ neighbourLabels nodeLabel
+    edgeList = map (\n -> (fst n, snd n, neighbours (fst n))) nodes
+    {-edgeList = [
+      ("node4",4,[8]),     -- the first component can be of any type
+      ("node8",8,[]),
+      ("node7",7,[4]),
+      ("node5",5,[1,7]),
+      ("node1",1,[4])
+     ] -}
+  in
+    G.graphFromEdges edgeList
 
--- next node to process 
-nextCandidateNode :: Graph -> Node
-nextCandidateNode (nodes', edges') = head $ sort $ findCandidateNodes (nodes', edges')
 
--- find the path in the given graph
-findPath :: Graph -> [Node]
-findPath (nodes, edges) = 
-  case (nodes,edges) of 
-    ([],_) -> []      -- no nodes left -> return empty list
-    (nodes', []) -> sort nodes' -- no edges left -> sort the remaining nodes, and return them
-    (nodes', edges') -> [firstNode] ++ findPath (removeNode (nodes', edges') firstNode)
-      where firstNode = nextCandidateNode (nodes', edges') 
+getFirst :: (a,b,c) -> a
+getFirst (a, _, _) = a
 
 -- solve part I: parse input, build graph, find the path and return it
 solveI :: String -> String
-solveI input = concat $ map (\(lbl, delay) -> lbl) $ findPath graph
-  where graph = buildGraph (parseInput input) 0
+solveI input = concat $ map (\n -> getFirst n) sorted
+  where (graph, vertexToNode, keyToVertex) = buildGraph (parseInput input) 
+        sorted = map vertexToNode $ G.topSort graph
 
--- given a list of workers, get the maximum remaining work
-getMaxRemainingWork :: [Worker] -> Int
-getMaxRemainingWork workers = maximum $ map remainingWork workers
-  where remainingWork (_, state) = case state of
-         Idle -> 0
-         Done _ -> 0
-         Working task since -> (snd task) - since  
+solveII = undefined
 
-availableWorkers :: [Worker] -> [Worker]
-availableWorkers workers= filter isAvailable workers
-  where isAvailable (_, state) = case state of 
-                                    Working _ _ -> False 
-                                    _ -> True
-
-assignNextTask :: SystemState -> SystemState
-assignNextTask (graph, workers, second) = 
-  let
-    nextTask = nextCandidateNode graph
-    nextWorker = head $ availableWorkers workers
-    (nwID, _)  = nextWorker
-    newWorker = (nwID, Working nextTask 0)
-    updatedWorkers = map (\(id, state) -> 
-                       if id /= nwID then (id, state)
-                       else newWorker 
-                     ) workers       
-  in
-    DT.trace ("assignNextTask: task = " ++ (show nextTask) ++ ", worker: " ++ (show newWorker)) (removeNode graph nextTask, updatedWorkers, second)    
-
-nextStepWorkers :: [Worker] -> [Worker]
-nextStepWorkers workers = DT.trace ("nextStepWorkers, before: " ++ (show workers) ++ ", after: " ++ (show newWorkers)) newWorkers
-  where nextStep (id, state) = case state of
-                                 Idle -> (id, state)
-                                 Done _ -> (id, state)
-                                 Working (lbl, delay) since -> if delay == since then (id, Done (lbl, delay))
-                                                               else (id, Working (lbl, delay) (since + 1))
-        newWorkers = (map nextStep workers)
-         
-
--- run simulation for part II step-by-step; terminate if work has been completed
-solveII' :: SystemState -> Int
-solveII' (graph, workers, second) = 
-  DT.trace ("solveII', second: " ++ (show second))(case (graph, availableWorkers workers) of
-                                                     (([], _), _) -> second + getMaxRemainingWork workers -- all nodes finished or being processed  
-                                                     ((nodes', edges'), []) -> solveII' (graph, nextStepWorkers workers, second + 1)
-                                                     ((nodes', edges'), w)  -> solveII' (assignNextTask (graph, workers, second)))
-
--- solve part II: parse input, build graph with offsets, run simulation step-by-step
-solveII :: String -> Int -> Int -> Int
-solveII input numWorkers offset = solveII' (graph, workers, 0) 
-  where graph = buildGraph (parseInput input) offset
-        workers = take numWorkers $ zip [1..] (repeat Idle)
-
-
+{-
 #if defined(STANDALONE)
 main = do
     input <- readFile "input.txt"
     putStrLn $ show $ solveI  input 
     --putStrLn $ show $ solveII input 
 #endif
-
+-}
