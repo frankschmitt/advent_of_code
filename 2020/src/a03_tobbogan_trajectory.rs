1789638f7bfc9075c215d7f6908cbba96ddb0252
Frank Schmitt <frankschmitt@gmx.de>
2020-12-13 17:02:02 +0100

    2020-03: tobbogan trajectory: solved part I

--- 2020/src/a03_tobbogan_trajectory.rs
+++ 2020/src/a03_tobbogan_trajectory.rs
@@ -1,5 +1,23 @@
 pub fn solve() {
-    let result1 = -1;
-    let result2 = -1;
+    // let filename = "a03_tobbogan_trajectory/test_input.txt";
+    let filename = "a03_tobbogan_trajectory/input.txt";
+    let grid = crate::helpers::read_char_grid(filename.to_string());
+    let mut result1 = 0;
+    let mut result2 = -1;
+    let mut pos = (0,0);
+    for i in 0 .. grid.height() {
+        let c = grid.cell(pos.0,pos.1);
+        if c == '#' {
+            println!("tree");
+            result1 += 1;
+        }
+        else {
+            println!("square");
+        }
+        pos.0 += 1;
+        pos.1 += 3;
+    }
+
+    println!("grid dim: height = {}, width = {}", grid.height(), grid.width());
     println!("03 - tobbogan trajectory: {} {}", result1, result2);
 }
--- 2020/src/helpers.rs
+++ 2020/src/helpers.rs
@@ -1,6 +1,46 @@
 use std::io::{BufRead, BufReader};
 use std::fs::File;
 
+pub struct Grid {
+    width: usize,
+    height: usize,
+    cells: Vec<Vec<char>>
+}
+
+
+// get the cell for the given row and column (wrapping around)
+impl Grid {
+    pub fn width(&self) -> usize {
+        return self.width;
+    }
+
+    pub fn height(&self) -> usize {
+        return self.height;
+    }
+
+    pub fn cell(&self, row: usize, col: usize) -> char {
+      let myrow = row % self.height;
+      let mycol = col % self.width;
+      println!("getting cell at {}/{} -> {}/{}", row, col, myrow, mycol);
+      let line = &self.cells[myrow];
+      let ch= line[mycol];
+      return ch;
+    }
+}
+
+pub fn read_char_grid(filename: String) -> Grid {
+   let lines = read_string_list(filename);
+   let mut cells = Vec::new();
+   for line in lines {
+      let chars: Vec<char> = line.chars().collect();
+      cells.push(chars);
+   }
+   let grid = Grid { height: cells.len(),
+                     width: cells[0].len(),
+                     cells: cells };
+   return grid;
+}
+
 pub fn read_int_list(filename: String) -> Vec<i64> {
     let reader = BufReader::new(File::open(filename).expect("Cannot open file"));
     let mut result = Vec::new();
