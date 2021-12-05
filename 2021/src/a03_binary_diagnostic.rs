47d1dc3660a1e3eeacd30e0f4310c2b44fc67000
Frank Schmitt <frankschmitt@gmx.de>
2021-12-05 15:25:45 +0100

    2021-03: binary diagnostic: cleanup code

--- 2021/src/a03_binary_diagnostic.rs
+++ 2021/src/a03_binary_diagnostic.rs
@@ -29,10 +29,9 @@ fn get_rating(mat: &Vec<Vec<char>>, rows: usize, cols: usize, rating_type: Ratin
       else { target_char = '1'; }
     }
     // only keep candidates with the correct target char in the current position
-    //candidates.retain (|&c| *mat[[c, current_col_idx]] == target_char);
     candidates.retain (|c| mat[*c][current_col_idx] == target_char);
-    println!("idx = {}, candidates: {:?}", current_col_idx, candidates);
-    print_candidates(&mat, &candidates);
+    // println!("idx = {}, candidates: {:?}", current_col_idx, candidates);
+    // print_candidates(&mat, &candidates);
     current_col_idx += 1;
     if candidates.len() == 1 { break; }
   }
@@ -57,18 +56,14 @@ pub fn solve() {
     let filename = "a03_binary_diagnostic/input.txt";
     //let filename = "a03_binary_diagnostic/example_input.txt";
     let v = crate::helpers::read_string_list((&filename).to_string());
-    // convert our vector of strings into a 2d-matrix of characters (each cell holds one character - either 0 or 1)
     let rows = v.len();
     let cols = v[0].len();
-    //let chars: Vec<char> = v.iter().map (|w| w.chars()).flatten().collect();
-    //let mat = Array::from_iter(chars.iter()).into_shape([rows, cols]).unwrap();
-    // part 1: for each column: count the number of zeroes and ones, and append the more frequent one to our result 
-    //let mat: Vec<char> = v.iter().map (|w| w.chars()).flatten().collect();
+    // convert our vector of strings into a 2d-matrix of characters (each cell holds one character - either 0 or 1)
     let mat: Vec<Vec<char>> = v.iter().map (|row| row.chars().collect()).collect();
+    // part 1: for each column: count the number of zeroes and ones, and append the more frequent one to our result 
     let mut gamma_s = String::new();
     let mut epsilon_s = String::new();
     for c in 0 .. cols {
-      //let col = mat.slice(s![.., c]);
       let col: Vec<char> = mat.iter().map(|row| row[c]).collect();
       let current_counts = col.iter().fold((0,0), |(c0, c1), elem| if *elem == '0' { (c0+1, c1)} else {(c0, c1+1)});
       if current_counts.0 > current_counts.1 { 
@@ -88,30 +83,7 @@ pub fn solve() {
     // part 2: successively filter the input lines by the current most (least) frequent bit until only one number remains
     // rather than removing whole strings, we just keep lists of the current candidates' indices
     let oxygen = get_rating(&mat, rows, cols, RatingType::OXYGEN);
-    let co2 = get_rating(&mat, rows, cols, RatingType::CO2);
-    
-    println!("oxygen: {}, co2: {}", oxygen, co2);
-    /*let mut oxygen_candidates: Vec<usize> = (0 .. rows).collect();
-    let mut idx_oxygen: usize = 0;
-    println!("mat: \n{:?}\n", mat);
-    println!("gamma {}, epsilon: {}", gamma_s, epsilon_s);
-    loop
-    { oxygen_candidates.retain (|&c| *mat[[c, idx_oxygen]] == gamma_chars[idx_oxygen]);
-      println!("idx = {}, oxygen_candidates: {:?}", idx_oxygen, oxygen_candidates);
-      print_candidates(&mat, &oxygen_candidates);
-      idx_oxygen += 1;
-      if oxygen_candidates.len() == 1 { break; }
-    }
-    let mut co2_candidates: HashSet<usize> = (0 .. rows).collect();
-    let mut idx_co2: usize = 0;
-    loop 
-    { 
-      co2_candidates.retain (|&c| *mat[[c, idx_co2]] == epsilon_chars[idx_co2]);
-      idx_co2 += 1;
-      if co2_candidates.len() == 1 { break; }
-    }
-    println!("oxygen: {:?}, co2: {:?}", oxygen_candidates, co2_candidates);
-    */
+    let co2 = get_rating(&mat, rows, cols, RatingType::CO2); 
     let result2 = oxygen * co2;
     println!("03 - binary diagnostic: {} {}", result1, result2);
 }
