8390cdb743809b2d511a6bcd5c67ce622cd4e03d
Frank Schmitt <frankschmitt@gmx.de>
2021-12-08 22:48:17 +0100

    2021-07: the treachery of whales: made solution robust against rounding errors (take mean_low and mean_high into account)

--- 2021/src/a07_the_treachery_of_whales.rs
+++ 2021/src/a07_the_treachery_of_whales.rs
@@ -1,5 +1,7 @@
+use std::cmp::min;
+
 pub fn solve() {
-    // let filename = "a07_the_treachery_of_whales/example_input.txt";
+    //let filename = "a07_the_treachery_of_whales/example_input.txt";
     let filename = "a07_the_treachery_of_whales/input.txt";
     let mut v = crate::helpers::read_int_comma_separated_list((&filename).to_string());
     // println!("{:?}", v);
@@ -10,8 +12,12 @@ pub fn solve() {
     let result1 = v.iter().fold(0, |acc, val| acc + (val - median).abs());
     // part II: take the mean (since it minimizes the sum of squared distances), compute the distance and the cost 
     //          the cost is the sum from i=1,...,n which is n(n+1)/2
-    let mean = ((v.iter().fold(0, |acc, val| acc + val) as f64) / (v.len() as f64)).round() as i64;
-    let result2 = v.iter().map(|val| (val-mean).abs()).fold(0, |acc, n| acc + (n*(n+1))/2);
-    // result2: 92676748 -> too high !?
+    let mean_low = ((v.iter().fold(0, |acc, val| acc + val) as f64) / (v.len() as f64)).floor() as i64;
+    let mean_high = ((v.iter().fold(0, |acc, val| acc + val) as f64) / (v.len() as f64)).ceil() as i64;
+    let result2_low  = v.iter().map(|val| (val-mean_low).abs()).fold(0, |acc, n| acc + (n*(n+1))/2);
+    let result2_high = v.iter().map(|val| (val-mean_high).abs()).fold(0, |acc, n| acc + (n*(n+1))/2);
+    let result2 = min(result2_low, result2_high);
+    //let result2b = v.iter().map(|val| (val-mean3).abs()).fold(0, |acc, n| acc + (n*(n+1))/2);
+    // result2: 92676748 -> too high !? with floor (result2a), we get 92676746 (but this gives the *wrong* result for the example input)
     println!("07 - the treachery of whales: {} {}", result1, result2);
 }
