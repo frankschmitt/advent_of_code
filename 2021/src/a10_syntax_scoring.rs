62a3feb13ca6c1f596790c7cc1dfc23711827150
Frank Schmitt <frankschmitt@gmx.de>
2021-12-10 19:05:34 +0100

    2021-10: syntax scoring: cleanup

--- 2021/src/a10_syntax_scoring.rs
+++ 2021/src/a10_syntax_scoring.rs
@@ -5,6 +5,7 @@ pub fn solve() {
     //let filename = "a10_syntax_scoring/example_input.txt";
     let v = crate::helpers::read_string_list((&filename).to_string());
 
+    // points for error
     let error_points = HashMap::from([ 
       (')', 3),
       (']', 57),
@@ -12,6 +13,7 @@ pub fn solve() {
       ('>', 25137)
     ]);
 
+    // points for line repairs
     // we use the opening delimiters so we can directly look up the point from the 
     let repair_points = HashMap::from([ 
         ('(', 1),
@@ -19,13 +21,7 @@ pub fn solve() {
         ('{', 3),
         ('<', 4)
       ]);
-    let repair_points2 = HashMap::from([ 
-        (')', 1),
-        (']', 2),
-        ('}', 3),
-        ('>', 4)
-      ]);
-
+    
     let opening_chars = vec!['(', '[', '{', '<' ];
     let matching_pairs = vec![('(', ')'), ('[', ']'), ('{', '}'), ('<', '>') ];
     let mut result1 = 0;
@@ -41,7 +37,7 @@ pub fn solve() {
                 let ch2 = stack.pop().unwrap();
                 if !matching_pairs.contains(&(ch2, ch)) {
                     result1 += error_points[&ch];
-                    println!("found error for {}, mismatch: {}", line, ch);
+                    // println!("found error for {}, mismatch: {}", line, ch);
                     error = true;
                     break;
                 }
@@ -55,12 +51,12 @@ pub fn solve() {
                 part_result2 *= 5;
                 part_result2 += repair_points[&ch];
             }
-            println!("repair score for {} : {}", line, part_result2);
+            // println!("repair score for {} : {}", line, part_result2);
             repair_scores.push(part_result2);
         }
     }
     repair_scores.sort();
-    println!("repair scores: {:?}", repair_scores);
+    // println!("repair scores: {:?}", repair_scores);
     let result2 = repair_scores[repair_scores.len() / 2];
     println!("10 - syntax scoring: {} {}", result1, result2);
 }
