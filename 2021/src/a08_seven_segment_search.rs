54d44a758afaa077addefd3cdb37ba28f4ece5cf
Frank Schmitt <frankschmitt@gmx.de>
2021-12-11 19:47:24 +0100

    2021-08: seven segment search: some progress on part II

--- 2021/src/a08_seven_segment_search.rs
+++ 2021/src/a08_seven_segment_search.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, BTreeSet};
+
 #[derive(Debug, Eq, PartialEq)]
 pub struct Entry {
     signal_patterns: Vec<String>,
@@ -11,20 +13,100 @@ pub fn parse_entry(s: &str) -> Entry {
     return Entry { signal_patterns, output_values };
 }
 
+fn is_subset_of(s1: &BTreeSet<char>, s2: &BTreeSet<char>) -> bool {
+    return s1.iter().all(|item| s2.contains(item));
+}
+
 pub fn solve() {
-    let filename = "a08_seven_segment_search/input.txt";
-    //let filename = "a08_seven_segment_search/example_input.txt";
+    //let filename = "a08_seven_segment_search/input.txt";
+    let filename = "a08_seven_segment_search/example_input.txt";
     let v = crate::helpers::read_string_list((&filename).to_string());
+    //let v = vec!["acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"];
     let entries: Vec<Entry> = v.iter().map(|line| parse_entry(line)).collect();
     // part I: simply call the output values corresponding to 1 (two segments), 4 (four segments), 7 (three segments) and 8 (seven segments)
     let mut matching_words = vec![];
-    for e in entries {
+    for e in entries.iter() {
         let mut its_matching_words: Vec<String> = e.output_values.iter().filter(|w| vec![2,4,3,7].contains(&w.len())).map(|s| s.to_string()).collect();
         matching_words.append(&mut its_matching_words);
     }
     // println!("matching words: {:?}", matching_words);
-    //let result1 = matching_word_lengths.fold(0, |acc, x| acc + x);
     let result1 = matching_words.len();
+
+    // part II is tricky; we use the following set of rules to determine which input string belongs to which input digit
+    //    length 2 => 1
+    //    length 3 => 4
+    //    length 4 => 7
+    //    length 7 => 8
+    //    length 6 and doesn't contain input for 1 => 6
+    //    length 6 and contains input for 4 => 9
+    //    length 6 and contains input for 1, but not for 4 => 0
+    //    length 5 and contains input for 1 => 3
+    //    length 5 and is contained in input for 6 => 5
+    //    length 5 and is not contained in input for 6 => 2
+    //  from that, we can determine the segments; here's the correct wiring
+    //    
+    //     aaaa 
+    //    b    c
+    //    b    c
+    //     dddd
+    //    e    f
+    //    e    f
+    //     gggg
+    //
+    //    segment a is the one that is set in 1 but not in 2
+    //    segment b is the one that is set in 9 but not in 3
+    //    segment c is the one that is not set in 6
+    //    segment d is the one that is not set in 0
+    //    segment e is the one that is not set in 9
+    //    segment f is the one that is set in all except 2
+    //    segment g is the one that is set for 0, 2, 3, 5, 6, 8, 9, but not for 1, 4, 7
+    for e in entries.iter() {
+        //let mut input_patterns = vec!["", "", "", "", "", "", "", "", "", ""];
+        let mut input_patterns = HashMap::<u8, BTreeSet::<char>>::new();
+        // 1, 4, 7 and 8 have unique lengths
+        input_patterns.insert(1, e.signal_patterns.iter().filter(|p| p.len() == 2).nth(0).unwrap().chars().collect());       
+        input_patterns.insert(4, e.signal_patterns.iter().filter(|p| p.len() == 4).nth(0).unwrap().chars().collect());       
+        input_patterns.insert(7, e.signal_patterns.iter().filter(|p| p.len() == 3).nth(0).unwrap().chars().collect());       
+        input_patterns.insert(8, e.signal_patterns.iter().filter(|p| p.len() == 7).nth(0).unwrap().chars().collect());       
+        // 6 is the string with length 6 that doesn't contain the pattern for 1
+        input_patterns.insert(6, e.signal_patterns.iter().filter(|p| {
+            let its_chars: BTreeSet::<char> = p.chars().collect(); 
+            return p.len() == 6 && !is_subset_of(&input_patterns[&1], &its_chars);
+         }
+        ).nth(0).unwrap().chars().collect());
+        // 9 is the string with length 6 that contains the pattern for 4
+        input_patterns.insert(9, e.signal_patterns.iter().filter(|p| {
+            let its_chars: BTreeSet::<char> = p.chars().collect(); 
+            return p.len() == 6 && is_subset_of(&input_patterns[&4], &its_chars);
+         }
+        ).nth(0).unwrap().chars().collect());
+        // 0 is the string with length 6 that contains the pattern for 1 but doesn' contain the pattern for 4
+        input_patterns.insert(0, e.signal_patterns.iter().filter(|p| {
+            let its_chars: BTreeSet::<char> = p.chars().collect(); 
+            return p.len() == 6 && is_subset_of(&input_patterns[&1], &its_chars) && !is_subset_of(&input_patterns[&4], &its_chars);
+         }
+        ).nth(0).unwrap().chars().collect());
+        // 3 is the string with length 5 that contains the pattern for 1
+        input_patterns.insert(3, e.signal_patterns.iter().filter(|p| {
+            let its_chars: BTreeSet::<char> = p.chars().collect(); 
+            return p.len() == 5 && is_subset_of(&input_patterns[&1], &its_chars);
+         }
+        ).nth(0).unwrap().chars().collect());
+        // 5 is the string with length 5 that is contained in the pattern for 6
+        input_patterns.insert(5, e.signal_patterns.iter().filter(|p| {
+            let its_chars: BTreeSet::<char> = p.chars().collect(); 
+            return p.len() == 5 && is_subset_of(&its_chars, &input_patterns[&6]);
+         }
+        ).nth(0).unwrap().chars().collect());
+        // 2 is the string with length 5 that is neither contained in the pattern for 6 nor contained in the pattern for 9
+        input_patterns.insert(2, e.signal_patterns.iter().filter(|p| {
+            let its_chars: BTreeSet::<char> = p.chars().collect(); 
+            return p.len() == 5 && !is_subset_of(&its_chars, &input_patterns[&6]) && !is_subset_of(&its_chars, &input_patterns[&9]);
+         }
+        ).nth(0).unwrap().chars().collect());
+        println!("input patterns for {:?} : {:?}", e, input_patterns);
+    }
+   
     let result2 = -1;
     println!("08 seven segment search: {} {}", result1, result2);
 }
