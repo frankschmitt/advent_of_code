bb367f6d9c818e0931a98f8a21e223c8d35670fa
Frank Schmitt <frankschmitt@gmx.de>
2022-12-06 22:48:03 +0100

    2022-05 supply stacks: solved part I

--- 2022/05-supply_stacks/SupplyStacks.py
+++ 2022/05-supply_stacks/SupplyStacks.py
@@ -1,6 +1,6 @@
 from parse import parse
 
-def parse_instruction(s):
+def parse_instruction_line(s):
     r = parse("move {:d} from {:d} to {:d}", s.rstrip())
     return {'n': int(r[0]), 'source': int(r[1]), 'target': int(r[2])}
 
@@ -23,14 +23,11 @@ class SupplyStacks:
         # read input, and split it into crates and instructions
         for line in lines:
             if mode == 'crate':
-                if line[1] == '1':
-                    mode = 'stack'
+                if line[1] == '1': # stack line; we just ignore it 
+                    mode = 'separator'
                     continue
                 else:
                     crate_lines.append(line)
-            elif mode == 'stack':
-                mode = 'separator'
-                continue
             elif mode == 'separator':
                 mode = 'instruction'
                 continue
@@ -42,14 +39,40 @@ class SupplyStacks:
             stack = parse_crate_line(cl)
             for key, value in stack.items():
                 if key in self.stacks:
-                    self.stacks[key].extend(value)
+                    # terrible hack - apparently, there's no prepend() in Python?
+                    tmp = value
+                    tmp.extend(self.stacks[key])
+                    self.stacks[key] = tmp
                 else:
                     self.stacks[key] = value
-        
+        self.instructions = []
+        for il in instruction_lines:
+            print("## parsing instruction: {}".format(il))
+            i = parse_instruction_line(il)
+            self.instructions.append(i)
 
-        
+    def execute_instruction(self, ins):
+        for i in range(0, ins['n']):
+            source = ins['source']
+            target = ins['target']
+            val = self.stacks[source].pop()
+            self.stacks[target].append(val)
+
+    def solve_part_I(self):
+        for ins in self.instructions:
+            print("executing instruction: {}".format(ins))
+            self.execute_instruction(ins)
+        res = ''
+        for idx, s in self.stacks.items():
+            res += s[-1]
+        return res
 
     def read_input_file(filename):
         with open(filename) as f:
             lines = f.readlines()
         return SupplyStacks(lines)
+
+
+if __name__ == '__main__':
+    ss = SupplyStacks.read_input_file('input.txt')
+    print("{} {}".format(ss.solve_part_I(), -1))
--- 2022/05-supply_stacks/SupplyStacks_Test.py
+++ 2022/05-supply_stacks/SupplyStacks_Test.py
@@ -3,9 +3,9 @@ import SupplyStacks as SS
 
 class SupplyStacksTest(unittest.TestCase):
 
-    def test_parse_instruction(self):
-        self.assertEqual({'n': 1, 'source': 2, 'target': 3}, SS.parse_instruction("move 1 from 2 to 3"))
-        self.assertEqual({'n': 99, 'source': 17, 'target': 105}, SS.parse_instruction("move 99 from 17 to 105\n"))
+    def test_parse_instruction_line(self):
+        self.assertEqual({'n': 1, 'source': 2, 'target': 3}, SS.parse_instruction_line("move 1 from 2 to 3"))
+        self.assertEqual({'n': 99, 'source': 17, 'target': 105}, SS.parse_instruction_line("move 99 from 17 to 105\n"))
 
     def test_parse_crate_line(self):
         self.assertEqual({1: [], 2: ['D']}, SS.parse_crate_line('    [D]'))
@@ -14,6 +14,19 @@ class SupplyStacksTest(unittest.TestCase):
     def test_read_example_input(self):
         ss = SS.SupplyStacks.read_input_file('example_input.txt')
         self.assertEqual({1: ['Z', 'N'], 2: ['M', 'C', 'D'], 3: ['P']}, ss.stacks)
+        self.assertEqual([{'n': 1, 'source': 2, 'target': 1},
+                          {'n': 3, 'source': 1, 'target': 3},
+                          {'n': 2, 'source': 2, 'target': 1},
+                          {'n': 1, 'source': 1, 'target': 2}],
+                         ss.instructions)
+
+    def test_solve_part_I(self):
+        ss = SS.SupplyStacks.read_input_file('example_input.txt')
+        self.assertEqual('CMZ', ss.solve_part_I())
+
+    def test_solve_part_II(self):
+        ss = SS.SupplyStacks.read_input_file('example_input.txt')
+        self.assertEqual('MCD', ss.solve_part_II())
 
 if __name__ == '__main__':
     unittest.main()
