35e9ab1aee94be633e17e126d55251b9b5da96a8
Frank Schmitt <frank.schmitt.fs1@contractors.roche.com>
2022-12-08 13:03:19 +0100

    2022-06: tuning trouble: solved part II

--- 2022/06-tuning_trouble/TuningTrouble.py
+++ 2022/06-tuning_trouble/TuningTrouble.py
@@ -10,26 +10,17 @@ class TuningTrouble:
 
     def solve(self, n):
         for i in range(0, len(self.line)-n+1):
-            duplicate = False
-            for j in range(i+1, i+n-1):
-                if self.line[i] == self.line[j]:
-                    duplicate = True 
-                    break
-            if duplicate == False:
+            distinct = set(self.line[i:i+n])
+            if len(distinct) == n:
                 return i+n
-        return -1
-
-    def solve_part_I_old(self):
-        for i in range(0, len(self.line)-3):
-            a, b, c, d = self.line[i], self.line[i+1], self.line[i+2], self.line[i+3]
-            if (a != b) and (a != c) and (a != d) and (b != c) and (b != d) and (c != d):
-                return i+4
         return -1
 
     def solve_part_I(self):
         return self.solve(4)
 
+    def solve_part_II(self):
+        return self.solve(14)
+
 if __name__ == '__main__':
-    #tt = TuningTrouble.read_input_file('example_input.txt')
     tt = TuningTrouble.read_input_file('input.txt')
-    print("{} {}".format(tt.solve_part_I(), tt.solve_part_I_old()))
+    print("{} {}".format(tt.solve_part_I(), tt.solve_part_II()))
