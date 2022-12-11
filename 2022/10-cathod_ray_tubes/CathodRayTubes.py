d7b422a3bd4532d55146c245c809f8125337f0eb
Frank Schmitt <frankschmitt@gmx.de>
2022-12-11 20:28:08 +0100

    2022-10: cathod ray tubes: solved part II

--- 2022/10-cathod_ray_tubes/CathodRayTubes.py
+++ 2022/10-cathod_ray_tubes/CathodRayTubes.py
@@ -13,32 +13,47 @@ class CathodRayTubes:
         self.lines = lines
         self.instructions = [parse_instruction(l) for l in lines]
         self.memory = [0] * 500 # will store actual values
+        self.output = [ ['.'] * 40 for i in range(0, 7)]
 
     def read_input_file(filename):
         with open(filename) as f:
             lines = [l.rstrip() for l in f.readlines()]
         return CathodRayTubes(lines)
 
-    def solve_part_I(self):
+    def print_sprite(self, i, msg, instruction, currval):
+        pos = i-1
+        sprite_range = range(currval-1, currval+2) # correct offset of 1
+        print("cycle {}: {} {}".format(i, msg, instruction))
+        if (pos % 40) in sprite_range:
+            self.output[pos // 40][pos % 40] = '#'
+        print("current CRT row: {}".format(''.join(self.output[pos // 40])))
+        sprite_pos_output = ['.'] * 40
+        sprite_pos_output[currval-1 : currval+1] = '###'
+        print("sprite pos:      {}".format(''.join(sprite_pos_output)))
+
+    def solve_it(self):
         currval = 1
         i = 1
         for ins in self.instructions: 
-            self.memory[i] = currval 
-            i += 1
-            if ins[0] == 'add':
+           self.memory[i] = currval 
+           self.print_sprite(i, "begin executing", ins, currval)
+           i += 1
+           if ins[0] == 'add':
                 # wait another cycle, and then perform the change
                 self.memory[i] = currval
+                self.print_sprite(i, "finish executing", ins, currval)
                 i += 1
                 currval += ins[1]
         # write result of last instruction
         self.memory[i] = currval
         result = sum(i * self.memory[i] for i in [20, 60, 100, 140, 180, 220])
+        # print part II
+        for l in self.output:
+            print(''.join(l))
         return result
-
-    def solve_part_II(self):
-        return -1
 
 if __name__ == '__main__':
     crt = CathodRayTubes.read_input_file('input.txt')
-    print("{} {}".format(crt.solve_part_I(), crt.solve_part_II()))
+    #crt = CathodRayTubes.read_input_file('example_input.txt')
+    print("{}".format(crt.solve_it()))
 
--- 2022/10-cathod_ray_tubes/CathodRayTubesTest.py
+++ 2022/10-cathod_ray_tubes/CathodRayTubesTest.py
@@ -7,16 +7,13 @@ class CathodRayTubesTest(unittest.TestCase):
     def test_simple_example(self):
         pass
         crt = CathodRayTubes.read_input_file('example_input-small.txt')
-        crt.solve_part_I()
+        crt.solve_it()
         got = [crt.memory[i] for i in [1, 2, 3, 4, 5, 6] ]
         self.assertEqual([1, 1, 1, 4, 4, -1], got) 
 
-    def test_solve_part_I(self):
+    def test_solve_it(self):
         crt = CathodRayTubes.read_input_file('example_input.txt')
-        self.assertEqual(13140, crt.solve_part_I())
+        self.assertEqual(13140, crt.solve_it())
 
 if __name__ == '__main__':
     unittest.main()
-
-
-# 1 + 15 - 11 + 6 - 3 + 5 - 1 - 8 + 13 + 4 = 21.)
