38c56664f1e3e064b1f4d991de0fb872dd5f4d0c
Frank Schmitt <frankschmitt@gmx.de>
2022-12-12 22:46:25 +0100

    2022-12: hill climbing algorithm: basic idea

---
+++ 2022/12-hill_climbing_algorithm/HillClimbingAlgorithm.py
@@ -0,0 +1,4 @@
+# Idea: build a directed graph
+#         vertices = grid squares
+#         edges = (v1, v2) where v1 is adjacent to v2 and height(v1) >= height(v2)-1
+#       find the shortest path from S to E  
