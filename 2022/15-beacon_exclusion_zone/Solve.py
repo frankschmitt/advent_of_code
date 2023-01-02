6e2a47f3c506065997b1b8bdf5a7b7b3b9a40283
Frank Schmitt <frankschmitt@gmx.de>
2023-01-02 17:38:32 +0100

    2022-15: beacon exclusion zone: introduced Sensor class (prep for part II)

--- 2022/15-beacon_exclusion_zone/Solve.py
+++ 2022/15-beacon_exclusion_zone/Solve.py
@@ -2,15 +2,28 @@ import logging
 import functools
 from parse import parse
 
+class Sensor:
+    def __init__(self, x, y, beacon_x, beacon_y):
+        self.x, self.y, self.beacon_x, self.beacon_y = x, y, beacon_x, beacon_y
+        self.range_ = abs(x - beacon_x) + abs(y - beacon_y)
+
+    def __eq__(self, other): 
+        if not isinstance(other, Sensor):
+            # don't attempt to compare against unrelated types
+            return NotImplemented
+
+        return self.x == other.x and self.y == other.y and self.beacon_x == other.beacon_x and self.beacon_y == other.beacon_y
+
+
 class Solve:
 
     def parse_line(self, line):
         # Sensor at x=2, y=18: closest beacon is at x=-2, y=15
         r = parse("Sensor at x={:d}, y={:d}: closest beacon is at x={:d}, y={:d}", line)
-        return [r[0], r[1], r[2], r[3]]
+        return Sensor(r[0], r[1], r[2], r[3])
 
     def __init__(self, lines):
-        self.beacons = [self.parse_line(line) for line in lines]
+        self.sensors = [self.parse_line(line) for line in lines]
 
     def read_input_file(filename):
         with open(filename) as f:
@@ -19,23 +32,27 @@ class Solve:
 
     def solve_part_I(self, row):
         forbidden = {}
-        for b in self.beacons:
-            range_ = abs(b[0]-b[2]) + abs(b[1]-b[3])
-            dist_to_row = abs(b[1] - row)
-            range_x = range_ - dist_to_row
-            logging.info("range for beacon:{} is {}, dist to row {} : {}".format(b, range_, row, dist_to_row))
-            for x in range(b[0]-range_x, b[0]+range_x):
-              logging.info("  beacon:{} removing {},in row {}".format(b, x, row))
+        for s in self.sensors:
+            dist_to_row = abs(s.y - row)
+            range_x = s.range_ - dist_to_row
+            logging.info("range for sensor:{} is {}, dist to row {} : {}".format(s, s.range_, row, dist_to_row))
+            for x in range(s.x-range_x, s.x+range_x):
+              logging.info("  sensor:{} removing {},in row {}".format(s, x, row))
               forbidden[x] = True
         return len(forbidden)
 
-    def solve_part_II(self):
+    def solve_part_II(self, max_x, max_y):
+        for x in range(0, max_x+1):
+            for y in range(0, max_y+1):
+                excluded = False
+                for b in self.sensors:
+                    pass 
         return -1
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     solve = Solve.read_input_file('input.txt')
     # part I: 5181556
-    print("{} {}".format(solve.solve_part_I(2000000), solve.solve_part_II()))
+    print("{} {}".format(solve.solve_part_I(2000000), solve.solve_part_II(4000000, 4000000)))
 
 
--- 2022/15-beacon_exclusion_zone/Solve_Test.py
+++ 2022/15-beacon_exclusion_zone/Solve_Test.py
@@ -1,6 +1,6 @@
 import unittest
 import logging
-from Solve import Solve
+from Solve import Solve, Sensor
 
 class SolveTest(unittest.TestCase):
     logger = logging.getLogger(__name__)
@@ -8,7 +8,7 @@ class SolveTest(unittest.TestCase):
   
     def test_parse_line(self):
         solve = Solve(["Sensor at x=2, y=18: closest beacon is at x=-2, y=15"])
-        self.assertEqual([[2, 18, -2, 15]], solve.beacons)
+        self.assertEqual([Sensor(2, 18, -2, 15)], solve.sensors)
 
     def test_solve_part_I(self):
         solve = Solve.read_input_file('example_input.txt')
@@ -16,7 +16,7 @@ class SolveTest(unittest.TestCase):
 
     def test_solve_part_II(self):
         solve = Solve.read_input_file('example_input.txt')
-        #self.assertEqual(-3, solve.solve_part_II())
+        self.assertEqual(56000011, solve.solve_part_II(20, 20))
 
 if __name__ == '__main__':
     unittest.main()
