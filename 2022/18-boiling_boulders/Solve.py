961b84807097fb3b9305f14cc7d4a8c89091fb55
Frank Schmitt <frankschmitt@gmx.de>
2023-01-04 22:33:14 +0100

    2022-18: boiling boulders: solved part I

--- 2022/18-boiling_boulders/Solve.py
+++ 2022/18-boiling_boulders/Solve.py
@@ -1,9 +1,33 @@
 import logging
 import functools
+import numpy as np
+
+# e.g. cube = (1,1,1) => faces are:
+#   (0,0,0), (0,1,0), (0,1,1), (0,0,1) # yz plane ("left")
+#   (1,0,0), (1,1,0), (1,1,1), (1,0,1) # yz plane ("right")
+#   (0,0,0), (1,0,0), (1,0,1), (0,0,1) # xz plane ("back")
+#   (0,1,0), (1,1,0), (1,1,1), (0,1,1) # xz plane ("front")
+#   (0,0,0), (1,0,0), (1,1,0), (0,1,0) # xy plane ("bottom")
+#   (0,0,1), (1,0,1), (1,1,1), (0,1,1) # xy plane ("top")
+class Cube:
+    def __init__(self, coords):
+        self.x, self.y, self.z = coords[0], coords[1], coords[2]
+        x, y, z = self.x, self.y, self.z
+        # we store each face in the format [p1, p2, p3, p4] and each p in format (x,y,z)
+        left  = [(x-1,y-1,z-1), (x-1,y,z-1), (x-1,y,z), (x-1,y-1,z)]  
+        right = [(x,y-1,z-1),   (x,y,z-1),   (x,y,z),   (x,y-1,z)]  
+        back  = [(x-1,y-1,z-1), (x,y-1,z-1), (x,y-1,z), (x-1,y-1,z)] 
+        front = [(x-1,y,z-1),   (x,y,z-1),   (x,y,z),   (x-1,y,z)] 
+        bottom= [(x-1,y-1,z-1), (x,y-1,z-1), (x,y,z-1), (x-1,y,z-1)]
+        top   = [(x-1,y-1,z),   (x,y-1,z),   (x,y,z),   (x-1,y,z)]
+        self.faces = [left, right, back, front, bottom, top]
 
 class Solve:
     def __init__(self, lines):
         self.lines = lines
+        self.cubes= [Cube(np.fromstring(s, dtype=int, sep=',')) for s in lines]
+        # build list of faces (6 per cube)
+        self.faces = [f for c in self.cubes for f in c.faces]
 
     def read_input_file(filename):
         with open(filename) as f:
@@ -17,13 +41,19 @@ class Solve:
         - return len(all_sides)
     """
     def solve_part_I(self):
-        return -1
+        unique_faces = {}
+        for f in self.faces:
+            if str(f) in unique_faces:
+                del unique_faces[str(f)]
+            else:
+                unique_faces[str(f)] = True # dummy value
+        return len(unique_faces)
 
     def solve_part_II(self):
         return -1
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     solve = Solve.read_input_file('input.txt')
     print("{} {}".format(solve.solve_part_I(), solve.solve_part_II()))
 
--- 2022/18-boiling_boulders/Solve_Test.py
+++ 2022/18-boiling_boulders/Solve_Test.py
@@ -1,15 +1,26 @@
 import unittest
 import logging
-from Solve import Solve
+import numpy as np
+from Solve import Solve, Cube
 
 class SolveTest(unittest.TestCase):
     logger = logging.getLogger(__name__)
     logging.basicConfig(level=logging.DEBUG)
   
 
+    def test_cube(self):
+        c = Cube([1,1,1])
+        self.assertEqual(6, len(c.faces))
+        self.assertEqual([(0,0,0), (0,1,0), (0,1,1), (0,0,1)], c.faces[0])
+
     def test_solve_part_I(self):
         solve = Solve.read_input_file('example_input.txt')
-        self.assertEqual(-2, solve.solve_part_I())
+        self.assertEqual(13, len(solve.cubes))
+        # check first cube
+        c0 = solve.cubes[0]
+        self.assertEqual((2, 2, 2), (c0.x, c0.y, c0.z))
+        self.assertEqual(13*6, len(solve.faces))
+        self.assertEqual(64, solve.solve_part_I())
 
     def test_solve_part_II(self):
         solve = Solve.read_input_file('example_input.txt')
