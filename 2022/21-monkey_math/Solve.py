c20fbd17e9c8a3387f17196ef3f392deae8f0414
Frank Schmitt <frankschmitt@gmx.de>
2023-01-06 11:23:28 +0100

    2022-21: monkey math: solved part I

--- 2022/21-monkey_math/Solve.py
+++ 2022/21-monkey_math/Solve.py
@@ -1,9 +1,27 @@
 import logging
 import functools
+from parse import parse
 
+class Monkey:
+    def __init__(self, line):
+        res = parse("{}: {}", line)
+        self.name = res[0]
+        res2 = parse("{} {} {}", res[1])
+        if res2 == None:
+            self.lhs = None
+            self.op = None
+            self.rhs = None
+            self.value = int(res[1])
+        else:
+            self.lhs = res2[0]
+            self.op = res2[1]
+            self.rhs = res2[2]
+            self.value = None
+        
 class Solve:
     def __init__(self, lines):
-        self.lines = lines
+        monkey_list = [Monkey(l) for l in lines]
+        self.monkeys = {m.name: m for m in monkey_list}
 
     def read_input_file(filename):
         with open(filename) as f:
@@ -11,7 +29,20 @@ class Solve:
         return Solve(lines)
 
     def solve_part_I(self):
-        return -1
+        result = -1
+        while result == -1:
+            for _, m in self.monkeys.items():
+                if m.value != None:
+                    continue
+                else:
+                    lhs = self.monkeys[m.lhs]
+                    rhs = self.monkeys[m.rhs]
+                    if lhs.value != None and rhs.value != None:
+                        val = eval("{} {} {}".format(lhs.value, m.op, rhs.value))
+                        m.value = int(val)
+                        if m.name == "root":
+                            result = m.value
+        return result
 
     def solve_part_II(self):
         return -1
--- 2022/21-monkey_math/Solve_Test.py
+++ 2022/21-monkey_math/Solve_Test.py
@@ -4,12 +4,29 @@ from Solve import Solve
 
 class SolveTest(unittest.TestCase):
     logger = logging.getLogger(__name__)
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
   
+    def test_parse_input(self):
+        solve = Solve.read_input_file('example_input.txt')
+        self.assertEqual(15, len(solve.monkeys))
+        # m0: root: pppw + sjmn
+        m0 = solve.monkeys["root"]
+        self.assertEqual("root", m0.name)
+        self.assertEqual("pppw", m0.lhs)
+        self.assertEqual("+", m0.op)
+        self.assertEqual("sjmn", m0.rhs)
+        self.assertEqual(None, m0.value)
+        # m1: dbpl: 5
+        m1 = solve.monkeys["dbpl"]
+        self.assertEqual("dbpl", m1.name)
+        self.assertEqual(None, m1.lhs)
+        self.assertEqual(None,  m1.op)
+        self.assertEqual(None, m1.rhs)
+        self.assertEqual(5, m1.value)
 
     def test_solve_part_I(self):
         solve = Solve.read_input_file('example_input.txt')
-        self.assertEqual(-2, solve.solve_part_I())
+        self.assertEqual(152, solve.solve_part_I())
 
     def test_solve_part_II(self):
         solve = Solve.read_input_file('example_input.txt')
