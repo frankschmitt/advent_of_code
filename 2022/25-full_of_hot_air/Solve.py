ac5400c489e08173165153bbb9d6f5cfa507bdf3
Frank Schmitt <frankschmitt@gmx.de>
2023-01-07 17:43:50 +0100

    2022-25: full of hot air: solved part I

--- 2022/25-full_of_hot_air/Solve.py
+++ 2022/25-full_of_hot_air/Solve.py
@@ -1,9 +1,85 @@
 import logging
 import functools
+from string import ascii_lowercase, digits
+
+snafu_vals = {
+        '2': 2,
+        '1': 1,
+        '0': 0,
+        '-': -1,
+        '=': -2
+}
+
+# convert num to its base 5 representation
+def to_base_5(num):
+    base = 5
+    sep = ','
+    glyphs = digits + ascii_lowercase
+    if num == 0:
+        return '0'
+    places = []
+    while num:
+        n, p = divmod(num, base)
+        places.append(p)
+        num = n
+    return ''.join([glyphs[p] for p in reversed(places)])
+
+def snafu_to_decimal(inp: str) -> int:
+    res = 0
+    exp = 0
+    for i in range(len(inp)-1, -1, -1):
+        res += snafu_vals[inp[i]] * 5**exp
+        exp += 1
+    return int(res) 
+
+def decimal_to_snafu(inp: int) -> str:
+    base5 = str(to_base_5(inp))
+    logging.info("input: {}, base5: {}".format(inp, base5))
+    # now, we need to handle 3 and 4; those aren't allowed in SNAFU. For each 3 we encounter
+    # we replace it with a 1= instead; a 4 is replaced by 1-
+    carry = 0
+    result = ""
+    for i in range(len(base5)-1, -1, -1):
+        logging.info("  pos {}, inp {}, carry {}".format(i, base5[i], carry))
+        if carry == 1:
+            logging.info(" CARRY == 1")
+            if base5[i] == '0':
+                result += '1'
+                carry = 0
+            elif base5[i] == '1':
+                result += '2'
+                carry = 0
+            elif base5[i] == '2':
+                result += '='
+                carry = 1
+            elif base5[i] == '3':
+                result += '-'
+                carry = 1
+            else:
+                result += '0'
+                carry = 1
+        else:
+            logging.info(" CARRY != 1")
+            if base5[i] == '3':
+                result += "="
+                carry = 1
+            elif base5[i] == '4':
+                result += "-"
+                carry = 1
+            else:
+                result += base5[i]
+                carry = 0
+        logging.info("  res: {}, carry: {}".format(result, carry))
+    if carry == 1:
+        logging.info("remaining carry: {}".format(carry))
+        result += '1'
+    logging.info("decimal_to_snafu: inp={}, base5={}, res={}".format(inp,base5,result[::-1]))
+    return result[::-1] # quirky syntax for reversing a string
+
 
 class Solve:
     def __init__(self, lines):
-        self.lines = lines
+        self.fuels = [snafu_to_decimal(l) for l in lines]
 
     def read_input_file(filename):
         with open(filename) as f:
@@ -11,7 +87,8 @@ class Solve:
         return Solve(lines)
 
     def solve_part_I(self):
-        return -1
+        logging.info("sum in decimal: {}".format(sum(self.fuels)))
+        return decimal_to_snafu(sum(self.fuels))
 
     def solve_part_II(self):
         return -1
--- 2022/25-full_of_hot_air/Solve_Test.py
+++ 2022/25-full_of_hot_air/Solve_Test.py
@@ -1,19 +1,45 @@
 import unittest
 import logging
-from Solve import Solve
+from Solve import Solve, snafu_to_decimal, decimal_to_snafu, to_base_5
 
 class SolveTest(unittest.TestCase):
     logger = logging.getLogger(__name__)
     logging.basicConfig(level=logging.DEBUG)
-  
 
-    def test_solve_part_I(self):
+    def xtest_to_base_5(self):
+        self.assertEqual("4", to_base_5(4))
+        self.assertEqual("10", to_base_5(5))
+        self.assertEqual("14", to_base_5(9))
+        self.assertEqual("21", to_base_5(11))
+        self.assertEqual("44", to_base_5(24))
+        self.assertEqual("100", to_base_5(25))
+ 
+    def xtest_snafu_to_decimal(self):
+        self.assertEqual(0, snafu_to_decimal("0"))
+        self.assertEqual(11, snafu_to_decimal("21"))
+        self.assertEqual(314159265, snafu_to_decimal("1121-1110-1=0"))
+
+    def test_decimal_to_snafu(self):
+        self.assertEqual("0", decimal_to_snafu(0))
+        self.assertEqual("1=", decimal_to_snafu(3))
+        self.assertEqual("1-", decimal_to_snafu(4))
+        self.assertEqual("10", decimal_to_snafu(5))
+        self.assertEqual("12", decimal_to_snafu(7))
+        self.assertEqual("2=", decimal_to_snafu(8))
+        self.assertEqual("2-", decimal_to_snafu(9))
+        self.assertEqual("1=0", decimal_to_snafu(15))
+        self.assertEqual(18, snafu_to_decimal(decimal_to_snafu(18)))
+        self.assertEqual("1-0---0", decimal_to_snafu(12345))
+        self.assertEqual(33038276688955, snafu_to_decimal(decimal_to_snafu(33038276688955)))
+             # "20=2==1101==2=022=10"))
+
+    def xtest_solve_part_I(self):
         solve = Solve.read_input_file('example_input.txt')
-        self.assertEqual(-2, solve.solve_part_I())
+        self.assertEqual("2=-1=0", solve.solve_part_I())
 
     def test_solve_part_II(self):
         solve = Solve.read_input_file('example_input.txt')
-        self.assertEqual(-3, solve.solve_part_II())
+        #self.assertEqual(-3, solve.solve_part_II())
 
 if __name__ == '__main__':
     unittest.main()
