f05c17a04c2692b238147fd24c885811a1f9296c
Frank Schmitt <frankschmitt@gmx.de>
2023-12-07 22:50:26 +0100

    2023-06: wait for it: solved part I

--- 2023/06-wait-for-it/Solve.py
+++ 2023/06-wait-for-it/Solve.py
@@ -1,13 +1,21 @@
 import logging
 import functools
 
+class Option:
+    def __init__(self, accelerate_ms, run_ms):
+        self.accelerate_ms, self.run_ms = accelerate_ms, run_ms
+        self.speed = self.accelerate_ms
+        self.distance_mm = self.speed * self.run_ms
+
+    def __str__(self):
+        return f"Option(acc: {self.accelerate_ms}, run: {self.run_ms}, dist: {self.distance_mm})"
+
 class Solve:
     def __init__(self, lines):
         times =     [ int(t) for t in lines[0].split()[1:] ]
         distances = [ int(t) for t in lines[1].split()[1:] ]
         logging.info(f"parsed input, times: {times}, distances: {distances}")
-        self.times = times
-        self.distances = distances
+        self.races = list(zip(times, distances))
 
     def read_input_file(filename):
         with open(filename) as f:
@@ -15,7 +23,13 @@ class Solve:
         return Solve(lines)
 
     def solve_part_I(self):
-        return -1
+        result = 1
+        for r in self.races:
+            options = [Option(i, r[0]-i) for i in range(0, r[0]+1)]
+            valid_options = [o for o in options if o.distance_mm > r[1] ]
+            logging.debug(f"valid option for {r[0]}/{r[1]}: {[str(vo) for vo in valid_options]}")
+            result *= len(valid_options)
+        return result 
 
     def solve_part_II(self):
         return -1
