c2fe1f020cc3daf1e7211a57b4fea28cfc04cbd8
Frank Schmitt <frankschmitt@gmx.de>
2023-12-10 22:05:41 +0100

    2023-10: pipe maze: basic setup

---
+++ 2023/10-pipe_maze/Solve_Test.py
@@ -0,0 +1,26 @@
+import unittest
+import logging
+from Solve import Solve
+
+class SolveTest(unittest.TestCase):
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(level=logging.DEBUG)
+  
+
+    def test_solve_part_I_simple_loop(self):
+        solve = Solve.read_input_file('example_input_1.txt')
+        self.assertEqual(4, solve.solve_part_I())
+
+    def test_solve_part_I_complex_loop(self):
+        #solve = Solve.read_input_file('example_input_2.txt')
+        #self.assertEqual(8, solve.solve_part_I())
+        pass
+
+    def test_solve_part_II(self):
+        #solve = Solve.read_input_file('example_input.txt')
+        #self.assertEqual(-3, solve.solve_part_II())
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
+
