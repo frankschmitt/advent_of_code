e5e40d16dd99f565c6903f8a1860946ca7421054
Frank Schmitt <frankschmitt@gmx.de>
2023-12-10 21:43:10 +0100

    2023-09: mirage maintenance: basic setup

---
+++ 2023/09-mirage_maintenance/Solve.py
@@ -0,0 +1,24 @@
+import logging
+import functools
+
+class Solve:
+    def __init__(self, lines):
+        self.lines = lines
+
+    def read_input_file(filename):
+        with open(filename) as f:
+            lines = [l.rstrip() for l in f.readlines()]
+        return Solve(lines)
+
+    def solve_part_I(self):
+        return -1
+
+    def solve_part_II(self):
+        return -1
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    solve = Solve.read_input_file('input.txt')
+    print("{} {}".format(solve.solve_part_I(), solve.solve_part_II()))
+
+
