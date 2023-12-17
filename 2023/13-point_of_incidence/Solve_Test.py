2caa89425d3d3910bf243349a5c1685a4b2d752b
Frank Schmitt <frankschmitt@gmx.de>
2023-12-17 21:03:53 +0100

    2023-13: point of incidence: basic setup

---
+++ 2023/13-point_of_incidence/Solve_Test.py
@@ -0,0 +1,20 @@
+import unittest
+import logging
+from Solve import Solve
+
+class SolveTest(unittest.TestCase):
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(level=logging.DEBUG)
+  
+
+    def test_solve_part_I(self):
+        solve = Solve.read_input_file('example_input.txt')
+        self.assertEqual(405, solve.solve_part_I())
+
+    def test_solve_part_II(self):
+        solve = Solve.read_input_file('example_input.txt')
+        self.assertEqual(-3, solve.solve_part_II())
+
+if __name__ == '__main__':
+    unittest.main()
+
