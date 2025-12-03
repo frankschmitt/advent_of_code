7169bfbbeeef95cb5353314b848521b4ca8ea4c9
Frank Schmitt <frankschmitt@gmx.de>
2025-12-03 23:12:55 +0100

    2025-02: gift shop: solved part II

--- 2025/02-gift-shop/Solve.py
+++ 2025/02-gift-shop/Solve.py
@@ -1,6 +1,12 @@
 import logging
 import functools
 
+# Source - https://stackoverflow.com/a/18854817
+# Posted by rlms, modified by community. See post 'Timeline' for change history
+# Retrieved 2025-12-03, License - CC BY-SA 3.0
+def chunkstring(string, length):
+   return (string[0+i:length+i] for i in range(0, len(string), length))
+
 class Solve:
     def __init__(self, lines):
         self.lines = lines
@@ -14,7 +20,9 @@ class Solve:
             lines = [l.rstrip() for l in f.readlines()]
         return Solve(lines)
 
-    def is_invalid(self, value):
+    # invalid check, part I
+    # we simply split the string in the middle and check whether both parts are equal
+    def is_invalid_part_I(self, value):
        s = str(value)
        s1 = s[0:len(s)//2]
        s2 = s[len(s)//2:]
@@ -22,18 +30,37 @@ class Solve:
        #logging.debug(f"is_invalid {value} : {s1} ? {s2} = {result}")
        return result
 
+    # invalid check, part II
+    # idea: split the string into chunks of the same length and check whether all chunks are equal
+    # we have to check chunks of length 1, 2, ..., len(s)//2
+    def is_invalid_part_II(self, value):
+       s = str(value)
+       logging.debug(f"value: {s}, length: {len(s)}, max chunk size: {len(s)//2}")
+       for i in range(1,len(s)//2 + 1):
+           chunks = list(chunkstring(s,i))
+           logging.debug(f"chunks of size {i}: {chunks}")
+           if all(c == chunks[0] for c in chunks):
+               logging.debug(f"{value} is invalid: {chunks}")
+               return True
+       logging.debug(f"{value} is valid")
+       return False
 
     def solve_part_I(self):
-        invalid_ids_list = [ [ v for v in r if self.is_invalid(v)] for r in self.ranges]
+        invalid_ids_list = [ [ v for v in r if self.is_invalid_part_I(v)] for r in self.ranges]
         invalid_ids = [v for row in invalid_ids_list for v in row]
         logging.debug(f"invalid ids: {invalid_ids}")
         #return functools.reduce(lambda x,acc: x*acc, invalid_ids, 1)
         return sum(invalid_ids)
 
     def solve_part_II(self):
-        return -1
+        invalid_ids_list = [ [ v for v in r if self.is_invalid_part_II(v)] for r in self.ranges]
+        invalid_ids = [v for row in invalid_ids_list for v in row]
+        logging.debug(f"invalid ids: {invalid_ids}")
+        #return functools.reduce(lambda x,acc: x*acc, invalid_ids, 1)
+        return sum(invalid_ids)
+
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     solve = Solve.read_input_file('input.txt')
     print("{} {}".format(solve.solve_part_I(), solve.solve_part_II()))
--- 2025/02-gift-shop/Solve_Test.py
+++ 2025/02-gift-shop/Solve_Test.py
@@ -6,11 +6,20 @@ class SolveTest(unittest.TestCase):
     logger = logging.getLogger(__name__)
     logging.basicConfig(level=logging.DEBUG)
 
-    def test_is_invalid(self):
+    def test_is_invalid_part_I(self):
         solve = Solve.read_input_file('example_input.txt')
-        self.assertEqual(True, solve.is_invalid(99))
-        self.assertEqual(True, solve.is_invalid(1212))
-        self.assertEqual(False, solve.is_invalid(12312))
+        self.assertEqual(True, solve.is_invalid_part_I(99))
+        self.assertEqual(True, solve.is_invalid_part_I(1212))
+        self.assertEqual(False, solve.is_invalid_part_I(12312))
+        self.assertEqual(False, solve.is_invalid_part_I(999))
+
+    def test_is_invalid_part_II(self):
+        solve = Solve.read_input_file('example_input.txt')
+        self.assertEqual(False, solve.is_invalid_part_II(12312))
+        self.assertEqual(True, solve.is_invalid_part_II(99))
+        self.assertEqual(True, solve.is_invalid_part_II(1212))
+        self.assertEqual(True, solve.is_invalid_part_II(999))
+
 
     def test_solve_part_I(self):
         solve = Solve.read_input_file('example_input.txt')
@@ -18,7 +27,7 @@ class SolveTest(unittest.TestCase):
 
     def test_solve_part_II(self):
         solve = Solve.read_input_file('example_input.txt')
-        self.assertEqual(-3, solve.solve_part_II())
+        self.assertEqual(4174379265, solve.solve_part_II())
 
 if __name__ == '__main__':
     unittest.main()
